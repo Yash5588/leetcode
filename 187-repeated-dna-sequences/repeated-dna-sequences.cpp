df205a6b8961d6adcacd485c53db1cad39f467a2
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-16 19:52:05 +0530

    Time: 36 ms (88.35%) | Memory: 18.2 MB (93.90%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
+    int MOD = 1e9+7;
+    int hashVal(char ch) {
+        switch(ch) {
+            case 'A' : return 0;
+            case 'C' : return 1;
+            case 'G' : return 2;
+            case 'T' : return 3;
+            default : return 0;
+        }
+    }
     vector<string> findRepeatedDnaSequences(string s) {
         int n = s.length();
         if(n <= 10) return {};
-        unordered_map<string,int> count;
-        string str = s.substr(0,10);
-        count[str]++;
-        vector<string> ans;
+        vector<int> power(10);
+        power[0] = 1;
+        int hash = 0;
+        int radix = 1;
+        for(int i=0;i<10;i++) {
+            radix = (radix * 4) % MOD;
+            hash = (hash + (hashVal(s[i])*radix) % MOD) % MOD;
+        }
+        unordered_map<int,int> hashcount;
+        hashcount[hash]++;
+        unordered_set<string> ans;
         for(int i=0;i<n-10;i++) {
-            str.erase(str.begin());
-            str += s[i+10];
-            if(count[str] != -1) {
-                count[str]++;
-            }
-            if(count[str] > 1) {
-                ans.push_back(str);
-                count[str] = -1;
+            hash /= 4;
+            hash -= hashVal(s[i]);
+            hash = (hash + (hashVal(s[i+10]) * radix) % MOD) % MOD;
+            hashcount[hash]++;
+            if(hashcount[hash] > 1) {
+                ans.insert(s.substr(i+1,10));
             }
         }
-        return ans;
+        return vector<string>(ans.begin(),ans.end());
+
     }
 };

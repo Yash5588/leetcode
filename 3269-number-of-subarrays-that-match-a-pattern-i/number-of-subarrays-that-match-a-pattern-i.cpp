dc2112a251f4df13ef603cf2cdb45b5c657f9b0c
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-20 09:05:41 +0530

    Time: 18 ms (26.42%) | Memory: 45.3 MB (5.85%) - LeetSync

--- 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.cpp
+++ 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    int MOD = 1e9+7;
+    vector<int> kmp(vector<int> &nums, vector<int> &p) {
+        string str;
+        for(auto &x : p) {
+            if(x == -1) str += '2';
+            else str += to_string(x);
+        }
+        str += '#';
+        for(int i=0;i<nums.size()-1;i++) {
+            if(nums[i] < nums[i+1]) str += '1';
+            else if(nums[i] > nums[i+1]) str += '2';
+            else str += '0'; 
+        }
+        vector<int> lps(str.size(),0);
+        for(int i=1;i<str.size();i++) {
+            int prev_index = lps[i-1];
+            while(prev_index>0 && str[i] != str[prev_index]) {
+                prev_index = lps[prev_index-1];
+            }
+            lps[i] = prev_index + (str[i] == str[prev_index]);
+        }
+        return lps;
+    }
     int countMatchingSubarrays(vector<int>& nums, vector<int>& p) {
-        int n = nums.size();
-        int m = p.size();
-        long long phash = 0, hash = 0;
-        long long radix = 1;
-        for(int i=0;i<n-1;i++) {
-            if(nums[i] < nums[i+1]) {
-                nums[i] = 1;
-            }
-            else if(nums[i] == nums[i+1]) {
-                nums[i] = 0;
-            }
-            else {
-                nums[i] = 2;
-            }
-        }
-        nums.resize(n-1);
-        for(int i=0;i<m;i++) {
-            if(p[m-i-1] == -1) p[m-i-1] = 2;
-            phash = ((phash + (p[m-i-1]*radix) % MOD) + MOD) % MOD;
-            hash = ((hash + (nums[m-i-1]*radix) % MOD) + MOD) % MOD;
-            radix = (radix * 3) % MOD;
-        }
-        long long count = 0;
-        count += (hash == phash);
-        for(int i=0;i<n-m;i++) {
-            hash = (hash * 3) % MOD;
-            hash = ((hash - (nums[i]*radix) % MOD) + MOD) % MOD;
-            hash = (hash + nums[i+m]) % MOD;
-            count += (hash == phash);
+        vector<int>lps = kmp(nums,p);
+        int plen = p.size();
+        int count = 0;
+        for(int i=plen;i<lps.size();i++) {
+            if(lps[i] == plen) count++;
         }
         return count;
     }

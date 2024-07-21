1c0927ded0c59fd048458ac360dcc18f2298de22
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-21 13:46:31 +0530

    Time: 30 ms (83.74%) | Memory: 21.6 MB (36.08%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.cpp
+++ 1508-longest-happy-prefix/longest-happy-prefix.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-    int MOD = 1e9+33;
-    string longestPrefix(string s) {
+    vector<int> kmp(string &s) {
         int n = s.length();
-        if(n <= 1) return "";
-        long long prefix = 0,suffix = 0;
-        unordered_map<char,int> codes;
-        for(char ch='a';ch<='z';ch++) {
-            codes[ch] = ch-'a'+1;
+        vector<int> lps(n,0);
+        for(int i=1;i<n;i++) {
+            int prev_index = lps[i-1];
+            while(prev_index>0 && s[prev_index] != s[i]) {
+                prev_index = lps[prev_index-1];
+            }
+            lps[i] = prev_index + (s[prev_index] == s[i]);
         }
-        int m = n-1;
-        string ans;
-        long long hash1 = codes[s[0]];
-        long long hash2 = codes[s[n-1]];
-        long long radix = 26;
-        long long ind = -1;
-        if(hash1 == hash2) ind = 0;
-        for(int i=1;i<m;i++) {
-            hash1 = (hash1 * 26) % MOD;
-            hash1 = (hash1 + codes[s[i]]) % MOD;
-            hash2 = ((hash2 + (codes[s[n-i-1]] * radix) % MOD) + MOD) % MOD;
-            radix = (radix * 26) % MOD;
-            // cout << hash1 << ' ' << hash2 << ' ' <<  i << endl;
-            if(hash1 == hash2)  ind = i;
-        }
-        ans = s.substr(0,ind+1);
-        return ans;
-        
-
+        return lps;
+    }
+    string longestPrefix(string s) {
+        vector<int> lps = kmp(s);
+        return s.substr(0,lps[s.length()-1]);
     }
 };

6659ad139867f963b0cf069821736cacb205996b
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-24 12:47:51 +0530

    Time: 460 ms (31.33%) | Memory: 135.5 MB (83.75%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -3,13 +3,13 @@ public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         int n = arr1.size();
         int m = arr2.size();
-        unordered_map<string,int> prefix;
+        unordered_map<int,int> prefix;
         for(auto &x : arr1) {
             string str;
             string num = to_string(x);
             for(int i=0;i<num.size();i++) {
                 str += num[i];
-                prefix[str] = i+1;
+                prefix[stoi(str)] = i+1;
             }
         }
         long long ma = INT_MIN;
@@ -19,10 +19,11 @@ public:
             long long c= 0;
             for(int i=0;i<num.size();i++) {
                 str += num[i];
-                if(prefix.find(str) == prefix.end()) {
+                int n = stoi(str);
+                if(prefix.find(n) == prefix.end()) {
                     break;
                 }
-                c = prefix[str];
+                c = prefix[n];
             }
             ma = max(ma,c);
         }

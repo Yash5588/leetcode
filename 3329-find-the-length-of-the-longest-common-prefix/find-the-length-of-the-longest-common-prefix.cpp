ee3771d6cc5b4a4a69d7e8d383c87220053a94f5
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-24 12:46:41 +0530

    Time: 487 ms (27.56%) | Memory: 170.4 MB (37.57%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,66 +1,32 @@
-struct Node{
-    Node* links[10];
-    int prefixCount = 0;
-
-    bool isContains(int ind) {
-        return links[ind] != nullptr;
-    }
-
-    void put(int ind, Node* node) {
-        links[ind] = node;
-    }
-
-    Node* get(int ind) {
-        return links[ind];
-    }
-};
-
-class Trie{
-    private:
-        Node* root = nullptr;
-    
-    public:
-        Trie() {
-            root = new Node();
-        }
-
-        void insert(int num) {
-            string str = to_string(num);
-            Node* node = root;
-            for(int i=0;i<str.size();i++) {
-                int ind = str[i]-'0';
-                if(!node->isContains(ind)) {
-                    node->put(ind, new Node());
-                }
-                node = node->get(ind);
-                node->prefixCount = i+1;
-            }
-        }
-
-        int prefix(int num) {
-            string str = to_string(num);
-            Node* node = root;
-            for(int i=0;i<str.length();i++) {
-                int ind = str[i]-'0';
-                if(!node->isContains(ind)) {
-                    return node->prefixCount;
-                }
-                node = node->get(ind);
-            }
-            return str.length();
-        }
-};
 class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        Trie trie;
+        int n = arr1.size();
+        int m = arr2.size();
+        unordered_map<string,int> prefix;
         for(auto &x : arr1) {
-            trie.insert(x);
+            string str;
+            string num = to_string(x);
+            for(int i=0;i<num.size();i++) {
+                str += num[i];
+                prefix[str] = i+1;
+            }
         }
-        int ma = INT_MIN;
+        long long ma = INT_MIN;
         for(auto &x : arr2) {
-            ma = max(ma, trie.prefix(x));
+            string str;
+            string num = to_string(x);
+            long long c= 0;
+            for(int i=0;i<num.size();i++) {
+                str += num[i];
+                if(prefix.find(str) == prefix.end()) {
+                    break;
+                }
+                c = prefix[str];
+            }
+            ma = max(ma,c);
         }
         return ma;
+        
     }
 };

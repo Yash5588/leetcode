8f04df502f8936f437f89d2c8229eb6c17300b54
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-24 17:39:09 +0530

    Time: 18 ms (17.80%) | Memory: 129 MB (5.27%) - LeetSync

---
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -0,0 +1,41 @@
+struct Node{
+    Node* links[26*26 + 26];
+    int count = 0;
+
+    bool isContains(int ind) {
+        return links[ind] != nullptr;
+    }
+
+    void put(int ind, Node* node) {
+        links[ind] = node;
+    }
+
+    Node* get(int ind) {
+        return links[ind];
+    }
+};
+class Solution {
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+        Node* root = new Node();
+        unordered_map<char,int> codes;
+        for(char ch='a';ch<='z';ch++) {
+            codes[ch] = ch-'a';
+        }
+        long long total = 0;
+        for(auto &s : words) {
+            int m = s.size();
+            Node* node = root;
+            for(int i=0;i<m;i++) {
+                int hash = codes[s[i]]*26 + codes[s[m-i-1]];
+                if(!node->isContains(hash)) {
+                    node->put(hash,new Node());
+                }
+                node = node->get(hash);
+                total += node->count;
+            }
+            node->count++;
+        }
+        return total;
+    }
+};

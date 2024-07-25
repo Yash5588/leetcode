25416f50401ccd267ce8a2c8d9e57e546acab6c2
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-25 14:45:43 +0530

    Time: 45 ms (92.12%) | Memory: 40.3 MB (90.62%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.cpp
+++ 654-maximum-binary-tree/maximum-binary-tree.cpp
@@ -11,16 +11,33 @@
  */
 class Solution {
 public:
-    TreeNode* createTree(vector<int> &a, int first, int last) {
-        if(first > last) return nullptr;
-        int i = max_element(a.begin()+first,a.begin()+last+1) - a.begin();
-        TreeNode* root = new TreeNode(a[i]);
-        root->left = createTree(a,first,i-1);
-        root->right = createTree(a,i+1,last);
-        return root;
-    }
+// this is divide and conquer
+    // TreeNode* createTree(vector<int> &a, int first, int last) {
+    //     if(first > last) return nullptr;
+    //     int i = max_element(a.begin()+first,a.begin()+last+1) - a.begin();
+    //     TreeNode* root = new TreeNode(a[i]);
+    //     root->left = createTree(a,first,i-1);
+    //     root->right = createTree(a,i+1,last);
+    //     return root;
+    // }
     TreeNode* constructMaximumBinaryTree(vector<int>& a) {
         int n = a.size();
-        return createTree(a,0,n-1);
+        // this is monotonic decreaing stack 
+        stack<TreeNode*> sta;
+        for(int i=0;i<n;i++) {
+            TreeNode* node = new TreeNode(a[i]);
+            TreeNode* prev = nullptr;
+            while(!sta.empty() && sta.top()->val <= a[i]) {
+                prev = sta.top();
+                sta.pop();
+            }
+            node->left = prev;
+            if(!sta.empty()) sta.top()->right = node; 
+            sta.push(node);
+        }
+        while(sta.size() != 1) {
+            sta.pop();
+        }
+        return sta.top();
     }
 };

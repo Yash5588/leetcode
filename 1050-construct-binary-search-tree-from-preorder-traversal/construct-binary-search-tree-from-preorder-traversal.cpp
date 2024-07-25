3fed1e439da0b5a64d336561e5d280b7eca2c376
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-25 20:53:08 +0530

    Time: 4 ms (56.16%) | Memory: 16.2 MB (25.78%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,20 +11,41 @@
  */
 class Solution {
 public:
-    int split(vector<int> &a, int first, int last) {
-        int ind = first+1;
-        while(ind < a.size() && a[ind] < a[first]) ind++;
-        return ind-1;
-    }
-    TreeNode* createTree(vector<int> &a, int first, int last) {
-        if(first > last) return nullptr;
-        TreeNode* root = new TreeNode(a[first]);
-        int ind = split(a,first,last);
-        root->left = createTree(a,first+1,ind);
-        root->right = createTree(a,ind+1,last);
-        return root;
-    }
+    // int split(vector<int> &a, int first, int last) {
+    //     int ind = first+1;
+    //     while(ind < a.size() && a[ind] < a[first]) ind++;
+    //     return ind-1;
+    // }
+    // TreeNode* createTree(vector<int> &a, int first, int last) {
+    //     if(first > last) return nullptr;
+    //     TreeNode* root = new TreeNode(a[first]);
+    //     int ind = split(a,first,last);
+    //     root->left = createTree(a,first+1,ind);
+    //     root->right = createTree(a,ind+1,last);
+    //     return root;
+    // }
     TreeNode* bstFromPreorder(vector<int>&a) {
-        return createTree(a,0,a.size()-1);
+        int n = a.size();
+        stack<TreeNode*> sta;
+        TreeNode* root = nullptr;
+        for(int i=0;i<n;i++) {
+            TreeNode* node = new TreeNode(a[i]);
+            if(i == 0) root = node;
+            TreeNode* prev = nullptr;
+            while(!sta.empty() && sta.top()->val <= a[i]) {
+                prev = sta.top();
+                sta.pop();
+            }
+            if(prev != nullptr) {
+                prev->right = node;
+            }
+            if(!sta.empty()) {
+                if(!sta.top()->left) {
+                    sta.top()->left = node;
+                }
+            }
+            sta.push(node);
+        }
+        return root;
     }
 };

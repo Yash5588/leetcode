a1def153380ce8840e48aebb2e8079526bef5473
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-25 14:09:21 +0530

    Time: 47 ms (87.62%) | Memory: 43 MB (36.05%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.cpp
+++ 654-maximum-binary-tree/maximum-binary-tree.cpp
@@ -13,16 +13,10 @@ class Solution {
 public:
     TreeNode* createTree(vector<int> &a, int first, int last) {
         if(first > last) return nullptr;
-        int ma = INT_MIN,ind = first;
-        for(int i=first;i<=last;i++) {
-            if(ma < a[i]) {
-                ma = a[i];
-                ind = i;
-            }
-        }
-        TreeNode* root = new TreeNode(ma);
-        root->left = createTree(a,first,ind-1);
-        root->right = createTree(a,ind+1,last);
+        int i = max_element(a.begin()+first,a.begin()+last+1) - a.begin();
+        TreeNode* root = new TreeNode(a[i]);
+        root->left = createTree(a,first,i-1);
+        root->right = createTree(a,i+1,last);
         return root;
     }
     TreeNode* constructMaximumBinaryTree(vector<int>& a) {

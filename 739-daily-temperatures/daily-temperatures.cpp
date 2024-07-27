59599346788c1dd17eeab0f2a5f56e4eb2ee6063
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-27 18:47:20 +0530

    Time: 131 ms (26.71%) | Memory: 101.2 MB (71.89%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     // 1) this problem is application of next greater element in array
     // 2) use monotically decreasing stack and iterate from backwards
+    // 3) The main Intuition is i need the number of elements popped by current also
+    // 4) for this thing i'm implementing pair which first stores value and second stores number
+    // of elements the current element has popped
+    // 5) while u pop anyelement make sure get the number of elements popped by stack top because those elements are already popped and we need their count this is the purpose
+    // 6) while pushing push with number of elements the current element has popped
     vector<int> dailyTemperatures(vector<int>& temp) {
         int n = temp.size();
         stack<pair<int,int>> sta;

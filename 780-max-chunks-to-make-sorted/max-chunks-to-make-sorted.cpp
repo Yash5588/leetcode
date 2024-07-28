c8839a0518cf3da655ec5e1f56a7236a66a2ac50
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-28 22:58:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (8.41%) - LeetSync

---
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        stack<int> sta;
+        for(int i=0; i<n;i++){
+            int ma = max(ma, arr[i]);
+            while(!sta.empty() && sta.top()> arr[i]){
+                sta.pop();
+            }
+            sta.push(ma);
+        }
+        
+        return sta.size();
+    }
+};

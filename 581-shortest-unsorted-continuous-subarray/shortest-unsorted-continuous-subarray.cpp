1cf6471cd1beebe73cd0ec194e43fc7efab34f8a
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-29 09:34:18 +0530

    Time: 18 ms (79.22%) | Memory: 30.3 MB (50.41%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
@@ -2,22 +2,36 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> temp= nums;
-        sort(nums.begin(),nums.end());
-        int ind1 = -1,ind2 = -1;
+        int  first = -1,last = -1;
+        stack<int> sta1,sta2;
+        //find previous greater element for lower boundary
         for(int i=0;i<n;i++) {
-            if(temp[i] != nums[i]) {
-                ind1 = i;
-                break;
+            //popping equal also because we need only greater element
+            while(!sta1.empty() && sta1.top() <= nums[i]) {
+                sta1.pop();
             }
+            if(!sta1.empty()) {
+                first = i;
+            // why i-1? because there is element which is greater than current element i so we need to consider i-1;
+            }
+            sta1.push(nums[i]);
         }
+        //need to find upper boundary so to find that we iterate from last and find next smaller element
         for(int i=n-1;i>=0;i--) {
-            if(temp[i] != nums[i]) {
-                ind2 = i;
-                break;
+            while(!sta2.empty() && sta2.top() >= nums[i]) {
+                sta2.pop();
             }
+            if(!sta2.empty()) {
+                last = i;
+            }
+            //why i+1? same as above reason we need to consider next element also
+            sta2.push(nums[i]);
         }
-        if(ind1 == -1) return 0;
-        return ind2-ind1+1;
+        if(last == -1) {
+            return 0;
+        }
+        if(first < last) return last-first+3;
+        return first-last+1;
+        
     }
 };

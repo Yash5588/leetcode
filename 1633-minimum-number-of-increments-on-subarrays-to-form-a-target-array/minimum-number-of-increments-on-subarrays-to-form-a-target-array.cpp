ea6f484f52d5542487315e82b286653a0bab7e21
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-07-31 10:13:24 +0530

    Time: 111 ms (7.54%) | Memory: 76.7 MB (16.79%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -2,11 +2,19 @@ class Solution {
 public:
     int minNumberOperations(vector<int>& target) {
         int n = target.size();
-        int ans = target[0];
-        for(int i=1;i<n;i++) {
-            if(target[i] > target[i-1]) {
-                ans += target[i] - target[i-1];
+        stack<int> sta;
+        sta.push(0);
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            bool flag = false;
+            while(!sta.empty() && sta.top() <= target[i]) {
+                if(!flag) {
+                    ans += target[i]-sta.top();
+                    flag = true;
+                }
+                sta.pop();
             }
+            sta.push(target[i]);
         }
         return ans;
     }

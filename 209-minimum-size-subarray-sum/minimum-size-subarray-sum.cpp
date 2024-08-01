b3a0fb514db917fffb84098e2557845b06093166
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-01 17:20:02 +0530

    Time: 28 ms (46.28%) | Memory: 31.8 MB (5.15%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int n = nums.size();
+        //prefix sum array
         vector<int> prefix(n+1,0);
         for(int i=1;i<=n;i++) {
             prefix[i] = prefix[i-1] + nums[i-1];
         }
+        //2-pointer sliding window
+        //if sum nums[last] - nums[first] exceeds target then we move first pointer to make it less and we move last pointer to make subarray sum equal to the target.
         int first = 0;
         int mi = INT_MAX;
         for(int last=0;last<=n;last++) {
             while((first<last) && (prefix[last] - prefix[first] >= target)) {
-                mi = min(mi, last-first);
+                mi = min(mi, last-first);  //at every increase in 1st pointer size of subarray decreases so we calculate min length of that subarray.
                 first++;
             }
         }

4c052604d25eece9f5ef7a537d76d23644c0f789
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-02 07:28:57 +0530

    Time: 290 ms (61.46%) | Memory: 147.4 MB (29.90%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -5,12 +5,14 @@ public:
         prefix = matrix;
         int m = matrix.size();
         int n = matrix[0].size();
+        //calculate prefix sum of matrix
         for(int i=1;i<m;i++) {
             prefix[i][0] += prefix[i-1][0];
         }
         for(int j=1;j<n;j++) {
             prefix[0][j] += prefix[0][j-1];
         }
+        //subtract diagonal becoz is added 2 times 
         for(int i=1;i<m;i++) {
             for(int j=1;j<n;j++) {
                 prefix[i][j] += prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
@@ -22,6 +24,7 @@ public:
         if(row1 == 0 && col1 == 0) return prefix[row2][col2];
         if(row1 == 0) return prefix[row2][col2] - prefix[row2][col1-1];
         if(col1 == 0) return prefix[row2][col2] - prefix[row1-1][col2];
+        // add diagonal becoz it is subtracted 2 times
         return prefix[row2][col2] - prefix[row1-1][col2] - prefix[row2][col1-1] + prefix[row1-1][col1-1];
     }
 };

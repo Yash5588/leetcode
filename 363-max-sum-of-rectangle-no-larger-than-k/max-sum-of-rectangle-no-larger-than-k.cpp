17e25635b4f4af67a1978f1d8dd3c7a64198f091
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-04 08:28:43 +0530

    Time: 647 ms (50.37%) | Memory: 242.2 MB (7.07%) - LeetSync

--- 363-max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-  int find(vector<int>& sum, int k) {
-    int result = INT_MIN;
-    set<int> se;
-    se.insert(0);
-    int prefixSum = 0;
-    for(int i = 0; i < sum.size(); i++) {
-      prefixSum += sum[i];
-
-      set<int>::iterator target = se.lower_bound(prefixSum - k);
-
-      if(target != se.end()) {
-        result = max(result, prefixSum - *target);
-      }
-      se.insert(prefixSum);
-    }
-
-    return result;
-  }
-  //Time: O(r2 * clogc)
-  // SC : O(c)
-  int maxSumSubmatrix(vector<vector<int>> &matrix, int k) {
-    int row = matrix.size();
-    int col = matrix[0].size();
-    int ma = INT_MIN;
-    for (int i = 0; i < row; i ++) {
-      vector<int> colSum(col,0);
-      for (int j = i; j < row; j ++) {
-        for (int c = 0; c < col; c ++) {
-          colSum[c] += matrix[j][c];
-          //System.out.print(colSum[c] + " ");
+    int Algo(vector<int> &arr, int k) {
+        int sum = 0, ma = INT_MIN;
+        set<int> se;
+        se.insert(0);
+        for(int i=0;i<arr.size();i++) {
+            sum += arr[i];
+            set<int> ::iterator iter = se.lower_bound(sum-k);
+            if(iter != se.end()) {
+                ma = max(ma, sum-*iter);
+            }
+            se.insert(sum);
         }
-        //System.out.println();
-        ma = max(ma, find(colSum, k));
-      }
+        return ma;
     }
-    return ma;
-  }
+    int maxSumSubmatrix(vector<vector<int>>& a, int k) {
+        int row = a.size();
+        int col = a[0].size();
+        int ma = INT_MIN;
+        for(int i=0;i<row;i++) {
+            vector<int> colSum(col);
+            for(int j=i;j<row;j++) {
+                for(int c=0;c<col;c++) {
+                    colSum[c] += a[j][c];
+                }
+                ma = max(ma, Algo(colSum,k));
+            }
+        }
+        return ma;
+    }
 };

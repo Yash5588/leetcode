55e715d32dfc91b97b35475f8655991c9548573e
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-06 06:23:01 +0530

    Time: 15 ms (72.36%) | Memory: 36 MB (7.65%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n+2,0),suffix(n+2,0);
+        for(int i=1;i<=n;i++) {
+            prefix[i] = prefix[i-1] + nums[i-1];
+        }
+        for(int i=n;i>=1;i--) {
+            suffix[i] = suffix[i+1] + nums[i-1];
+        }
+        for(int i=1;i<=n;i++) {
+            if(prefix[i-1] == suffix[i+1]) return i-1;
+        }
+        return -1;
+    }
+};

bfa05e0b67c4a11c28fd947cc32b970f3b380b25
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-15 09:37:08 +0530

    Time: 35 ms (37.02%) | Memory: 42.4 MB (6.15%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int n = nums.size();
+        //for maintaining the count for prefix sum;
         unordered_map<long long, long long> mp;
         long long sum = 0;
         long long ans = 0;
         mp[0] = 1;
         for(int i=0;i<n;i++) {
+            //prefix sum
             sum += nums[i];
+
+            //getting the sum-goal count
             ans += mp[sum-goal];
+
+            //updating current sum count
             mp[sum]++;
         }
         return ans;

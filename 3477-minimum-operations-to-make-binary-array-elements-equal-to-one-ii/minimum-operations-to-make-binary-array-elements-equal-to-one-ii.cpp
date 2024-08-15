eb8c19b177ef2a000f8259fc9fba75ed1047a119
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-15 13:00:59 +0530

    Time: 125 ms (49.99%) | Memory: 155.7 MB (6.33%) - LeetSync

--- 3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/minimum-operations-to-make-binary-array-elements-equal-to-one-ii.cpp
+++ 3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/minimum-operations-to-make-binary-array-elements-equal-to-one-ii.cpp
@@ -3,10 +3,14 @@ public:
     int minOperations(vector<int>& nums) {
         int n = nums.size();
         int flips = 0;
+        //keep track of all previous splits
         for(int i=0;i<n;i++) {
+            //if current bit is '0' and previous flips are even then the bit will remain same to avoid that we will perform one more flip
             if(nums[i] == 0 && flips%2 == 0) {
                 flips++;
             }
+
+            //if current bit is '1' and previous flips are odd then 1 changes to 0 we don't need that so flip it
             else if(nums[i] == 1 && flips%2 != 0) {
                 flips++;
             }

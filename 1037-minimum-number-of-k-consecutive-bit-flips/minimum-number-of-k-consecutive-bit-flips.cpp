b6528e20d15e8b68b714ad87fc45730fb5e923a1
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-15 13:58:52 +0530

    Time: 83 ms (71.14%) | Memory: 109.2 MB (65.58%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -2,9 +2,16 @@ class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
         int n = nums.size();
+        //for counting the previous flips for each window
         long long flips = 0;
+
+        //flips in overall array
         long long ans = 0;
+
+        //to keep track which index bit has contributed for flipping so that when window moves we can remove those flips
         vector<bool> isFlipped(n+1,false);
+
+        //building the window
         for(int i=0;i<k;i++) {
             if(nums[i] == 0 && flips % 2 == 0) {
                 flips++;
@@ -17,6 +24,9 @@ public:
                 isFlipped[i] = true;
             }
         }
+
+        //window is moving remove flips for i and add for i+k becoz of sliding window technique 
+        //keep track of overall flips
         for(int i=0;i<n-k;i++) {
             if(isFlipped[i]) flips--;
             if(nums[i+k] == 0 && flips % 2 == 0) {
@@ -30,6 +40,9 @@ public:
                 isFlipped[i+k] = true;
             }
         }
+
+        //This is a zone where we can't apply flipping so from
+        //n-k -> n if we need to apply a flip we can't so we can't convert whole 1's in the end see question and understand.
         for(int i=n-k+1;i<n;i++) {
             if(isFlipped[i]) return -1;
         }

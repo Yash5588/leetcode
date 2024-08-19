de6f121732cd5349ba456320b5b43c1133fab557
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-19 17:25:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (61.54%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -14,6 +14,7 @@ public:
                 swap(nums[first],nums[last]);
             }
         }
+        //for empty array and if array contains all elements as val ex: [2,2,2] val = 2 edge case
         if(last >= 0 && nums[last] == val) return 0;
         return last+1;
     }

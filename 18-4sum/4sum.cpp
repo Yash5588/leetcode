7d9ccc2ee53a837c035487294d340a3d4240f1b5
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-19 23:55:25 +0530

    Time: 303 ms (10.58%) | Memory: 82.8 MB (8.66%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -4,15 +4,18 @@ public:
         int n = nums.size();
         sort(nums.begin(),nums.end());
         set<vector<int>> se;
+        //we will fix 2-pointers and the rest will be taken care by 2-pointer approach
         for(int d = 0;d < n-3; d++) {
             for(int c = d+1;c < n-2;c++) {
                 int a = c+1, b = n-1;
                 while(a < b) {
                     long long sum = nums[a];
+                    //this to addess the integer overflow i added them seperately
                     sum += nums[b];
                     sum += nums[c];
                     sum += nums[d];
                     if(sum == target) {
+                        //store in set for unique combinations
                         se.insert({nums[a],nums[b],nums[c],nums[d]});
                     }
                     if(sum < target) a++;

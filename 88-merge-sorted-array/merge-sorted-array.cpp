ea8f37ef9da5c2b5d07f04559024ae42d215f7ac
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-19 22:25:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (45.58%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        //'i' point to last to last element in nums1, 'j' point
+        //to last element in nums2 
+        //curr to last index in nums1 where we can fill
         int i = m-1,j = n-1,curr = m+n-1;
         while(i >= 0 && j >= 0) {
             if(nums1[i] >= nums2[j]) {
@@ -14,6 +17,8 @@ public:
                 j--;
             }
         }
+        //if elements are left in nums2 then assign them
+        //if elements are left in nums1 then they are anyway in nums1 itself
         while(j >= 0) {
             nums1[curr] = nums2[j];
             curr--;

de0e0945915befc85836dada3c303b55c34cefdc
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-20 08:07:36 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (36.31%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -3,20 +3,30 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int ind = -1;
+        //for next permutation we need a break point
+        //such that it should be less than next element
+        //specifically find that point from last
         for(int i=n-2;i>=0;i--) {
+            //check with next element
             if(nums[i] < nums[i+1]) {
                 ind = i;
                 break;
             }
         }
+        //checking if there is no break point(decreasing array)
         if(ind != -1) {
+            //swap with greater element such that it should be smallest and it should be greater than break point index
             for(int i=n-1;i>ind;i--) {
                 if(nums[ind] < nums[i]) {
+                    //if you find swap them
                     swap(nums[i],nums[ind]);
                     break;
                 }
             }
         }
+        //anyway after break point we observe that it is sorted in decreasing order
+        //we need it the smallest
+        // we need it in ascending order so we reverse it
         reverse(nums.begin()+ind+1,nums.end());
     }
 };

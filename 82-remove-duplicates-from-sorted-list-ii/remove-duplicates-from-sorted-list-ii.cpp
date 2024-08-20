b742ed1be09ebe0ec66d72fdf70235d111279b72
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-20 19:55:01 +0530

    Time: 3 ms (88.31%) | Memory: 15.6 MB (35.35%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -14,23 +14,31 @@ public:
         if(head == nullptr) return nullptr;
         ListNode* first = head;
         ListNode* last = head;
+        //count each occurance and point last pointer at last occurance
         while(last != nullptr) {
             int count = 1;
             while(last->next != nullptr && last->val == last->next->val) {
                 count++;
                 last = last->next;
             }
+            //if count of that occurance is 1 then only we will assign
+            //last to first otherwise ignore those nodes
             if(count == 1) {
                 first->val = last->val;
                 first = first->next;
             }
             last = last->next;
         }
+        //edge case when list is [1,1,2,2] no node has 1 occurance
+        //then basically remove whole list
         if(first == head) return nullptr;
+
+        //shrink the list for that we need to remove list from where first is pointing
         ListNode* temp = head;
         while(temp->next != first) {
             temp = temp->next;
         }
+        //make the node which is pointed by first pointer as NULL
         temp->next = nullptr;
         return head;
     }

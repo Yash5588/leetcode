81c656d9e0e29b46d7b7288c06699892b48913ad
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-23 05:43:58 +0530

    Time: 2177 ms (5.07%) | Memory: 497.3 MB (5.76%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> bucket(1000001,0);
+        vector<int> diff;
         for(int i=0;i<n-1;i++) {
             for(int j=i+1;j<n;j++) {
-                bucket[abs(nums[i]-nums[j])]++;
+                diff.push_back(abs(nums[i]-nums[j]));
             }
         }
-        for(int i=0;i<=1000000;i++) {
-            k -= bucket[i];
-            if(k <= 0) return i;
-        }
-        return 0;
+        nth_element(diff.begin(),diff.begin()+(k-1),diff.end());
+        return diff[k-1];
     }
 };

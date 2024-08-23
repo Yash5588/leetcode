26ef637cb92e7b39595cd0e37a152d4c2f42d9fa
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-23 08:09:25 +0530

    Time: 11 ms (45.20%) | Memory: 12.7 MB (91.93%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
+    int getPairs(vector<int> &nums, int dist) {
+        int i = 0,n = nums.size();
+        int pairs = 0;
+        for(int j=1;j<n;j++) {
+            while(i < j && nums[j] - nums[i] > dist) {
+                i++;
+            }
+            pairs += (j-i);
+        }
+        return pairs;
+    }
     int smallestDistancePair(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> diff;
-        for(int i=0;i<n-1;i++) {
-            for(int j=i+1;j<n;j++) {
-                diff.push_back(abs(nums[i]-nums[j]));
+        sort(nums.begin(),nums.end());
+        int left = 0;
+        int right = nums[n-1] - nums[0];
+        int result = 0;
+        while(left <= right) {
+            int midist = left + (right-left)/2;
+            int pairs = getPairs(nums,midist);
+            if(pairs < k)  left = midist+1;
+            else {
+                result = midist;
+                right = midist-1;
             }
         }
-        nth_element(diff.begin(),diff.begin()+(k-1),diff.end());
-        return diff[k-1];
+        return result;
     }
 };

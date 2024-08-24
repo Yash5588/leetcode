58ce98c2eb988b1df7660f6808b5413fe23738a8
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-24 06:05:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (86.57%) - LeetSync

--- 827-expressive-words/expressive-words.cpp
+++ 827-expressive-words/expressive-words.cpp
@@ -8,30 +8,38 @@ public:
             int i = 0,j = 0;
             bool flag = true;
             while(j < m) {
+                //check each group count
                 int count1 = 0,count2 = 0;
                 while(j < m && s[i] == str[j]) {
                     j++;
                     count2++;
                 }
+                //if nothing matches break
                 if(count2 == 0) {
                     flag = false;
                     break;
                 }
+                //move other pointer also
                 while(i < n && s[i] == str[j-1]) {
                     i++;
                     count1++;
                 }
+                //compare that group count from both strings
                 if(count1 > count2) {
                     if(count1 < 3) {
+                        //if it is less than 3 break
                         flag = false;
                         break;
                     }
                 }
                 if(count1 < count2) {
+                    //if main string count is less than words strings break
                     flag = false;
                     break;
                 }
             }
+            //if both strings reached end and also satisfies all conditions
+            //then consider that string in ans
             if(i == n && j == m && flag) ans++;
         }
         return ans;

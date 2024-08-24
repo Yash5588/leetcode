cd4ad8229993a91125927866eee7b8435ead8c39
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-24 07:07:18 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (50.34%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -2,8 +2,13 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         int n = chars.size();
+        //i and j for group checking
+        //k is to form array
         int i=0,j=1,k=0;
         if(n == 1) return 1;
+        //edge case if last group has only one character then it will
+        //not be considered in below snippet
+        //so we are checking initially before manipulating the original array
         bool flag = false;
         if(chars[n-1] != chars[n-2]) flag = true; 
         while(j < n) {
@@ -13,15 +18,19 @@ public:
             }
             k++;
             if(j-i != 1) {
+                //if count > 1 and has more digits assign all in array
                 string str = to_string(j-i);
                 for(auto &x : str) {
                     chars[k] = x;
                     k++;
                 }
             }
+            //again check for next group
             i = j;
             j++;
         }
+        //The above mentioned edge case 
+        //ex : ["a","b","c"],["b","b","b","c"] last group count is 1 for that case
         if(flag) {
             chars[k] = chars.back();
             k++;

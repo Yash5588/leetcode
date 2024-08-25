d0cdff267cb46e2fd474af4b9fe97fcfa68dadf1
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-25 06:39:02 +0530

    Time: 3 ms (97.72%) | Memory: 10.3 MB (60.88%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -14,9 +14,12 @@ public:
         while(fast != nullptr && fast->next != nullptr) {
             slow = slow->next;
             fast = fast->next->next;
+            //when the slow and fast pointers meet it is not necessarily that it is the start of cycle
             if(slow == fast) {
                 fast = head;
+                //we move fast to start again
                 while(slow != fast) {
+                    //we move 1 step and wherever they meet that is the start point of cycle.
                     slow = slow->next;
                     fast = fast->next;
                 }

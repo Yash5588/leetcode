b842c9203d5b596c2c6c4f343edfb923d1c9a3a4
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-08-26 06:34:15 +0530

    Time: 4 ms (83.76%) | Memory: 9 MB (79.90%) - LeetSync

--- 481-magical-string/magical-string.cpp
+++ 481-magical-string/magical-string.cpp
@@ -2,8 +2,12 @@ class Solution {
 public:
     int magicalString(int n) {
         string str;
+        //if the length is <= 3, 1 is present only once
         if(n <= 3) return 1;
+        //initial string
         str += "122";
+        //i = points to the current string
+        //j = points to the last character and adds str[i] number of characters to the string
         int i = 2,j = 2;
         while(j < n-1) {
             if(str[i] == '1') {
@@ -19,6 +23,7 @@ public:
             }
             i++;
         }
+        //return count
         return count(str.begin(),str.end(),'1');
     }
 };

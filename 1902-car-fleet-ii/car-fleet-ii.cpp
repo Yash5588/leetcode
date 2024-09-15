3e7eaa5cc388594b394ed49252f53fcfd7e762a4
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-09-15 22:43:05 +0530

    Time: 390 ms (55.90%) | Memory: 134.8 MB (68.40%) - LeetSync

--- 1902-car-fleet-ii/car-fleet-ii.cpp
+++ 1902-car-fleet-ii/car-fleet-ii.cpp
@@ -5,11 +5,21 @@ public:
         vector<double> res(n);
         stack<int> sta;
         for(int i = n-1;i >= 0;i--) {
+            //check what is the next car we can collide
             while(!sta.empty() && cars[sta.top()][1] >= cars[i][1]) {
                 sta.pop();
             }
+            //if no car found
             if(sta.empty()) res[i] = -1;
             else {
+                //but even if we find the next car
+                //there is a chance that it's collision time is less than 
+                //current car collision time
+                //like if our current car collision time with that car is 3sec
+                //and that other car collision time is 1.5sec
+                //then think logically it will collide 1st right before we collide
+                //it so we have to collide with the car with which this 1.5sec car
+                //collide so remove lesser time than current car collision from stack
                 while(!sta.empty()) {
                     double dist = cars[sta.top()][0] - cars[i][0];
                     double speed = cars[i][1] - cars[sta.top()][1];

cba8968250a7456befb365bfcadb89460af4d885
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-09-16 10:41:05 +0530

    Time: 1293 ms (7.69%) | Memory: 557.4 MB (7.69%) - LeetSync

--- 3559-minimum-number-of-valid-strings-to-form-target-i/minimum-number-of-valid-strings-to-form-target-i.cpp
+++ 3559-minimum-number-of-valid-strings-to-form-target-i/minimum-number-of-valid-strings-to-form-target-i.cpp
@@ -37,6 +37,7 @@ class Trie {
             }
         }
 
+        //gets max possible prefix from trie which matches target
         int prefix(string &word) {
             Node* node = root;
             int n = word.length();
@@ -56,13 +57,23 @@ public:
     int dp[10000];
     int solve(string &target, Trie trie, int pos) {
         int n = target.size();
+        //if we reach end safely then it is a 
+        //valid route
         if(pos == n) return 0;
+        //if we cross then it is not valid route
         if(pos > n) return 10000;
         if(dp[pos] != -1) return dp[pos];
         string str = target.substr(pos);
+        //check max prefix length matching strings in words
+        //which are inserted in trie
         int len = trie.prefix(str);
         int mi = 10000;
+        //we won't include while length
+        //we check by including varous lengths
         for(int l = 1;l <= len;l++) {
+            //and check which route is giving min value
+            //adding one becoz we are considering current words
+            //by including l length prefix characters
             mi = min(mi, 1 + solve(target,trie,pos+l));
         }
         return dp[pos] = mi;

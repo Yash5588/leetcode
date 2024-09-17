6cc7acfca219815c379a6144d41c846a14b3fb01
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-09-17 21:08:58 +0530

    Time: 140 ms (18.07%) | Memory: 296.8 MB (16.82%) - LeetSync

---
+++ 1692-number-of-ways-to-reorder-array-to-get-same-bst/number-of-ways-to-reorder-array-to-get-same-bst.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    long long postorder(vector<int> &nums, vector<vector<long long>> &ncr) {
+        int n = nums.size();
+        if(n == 0) return 1;
+        vector<int> left, right;
+        for(int i = 1;i < n;i++) {
+            if(nums[i] < nums[0]) left.push_back(nums[i]);
+            else right.push_back(nums[i]);
+        }
+        int r = left.size();
+        long long left_arrange = postorder(left,ncr)%mod;
+        long long right_arrange = postorder(right,ncr)%mod;
+        long long res = ((ncr[n-1][r] * left_arrange)%mod * right_arrange)%mod;
+        return res;
+    }
+    int numOfWays(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<long long>> ncr(n+1,vector<long long> (n+1,1));
+        for(int i = 1;i <= n;i++) {
+            for(int j = 1;j < i;j++) {
+                ncr[i][j] = ((ncr[i-1][j-1])%mod + (ncr[i-1][j])%mod)%mod;
+            }
+        }
+        long long ans = postorder(nums,ncr)%mod;
+        // for(int i = 0;i <= n;i++) {
+        //     for(int j = 0;j <= i;j++) {
+        //         cout << i << ' ' << j << ' ' << ncr[i][j] << endl;
+        //     }
+        // }
+        return ans - 1;
+    }
+};

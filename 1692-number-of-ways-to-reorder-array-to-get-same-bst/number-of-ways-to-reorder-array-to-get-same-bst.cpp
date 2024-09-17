93eb87d200aa99e952874c5ce2a496b47465ac0b
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-09-17 21:13:18 +0530

    Time: 159 ms (12.77%) | Memory: 296.9 MB (16.82%) - LeetSync

--- 1692-number-of-ways-to-reorder-array-to-get-same-bst/number-of-ways-to-reorder-array-to-get-same-bst.cpp
+++ 1692-number-of-ways-to-reorder-array-to-get-same-bst/number-of-ways-to-reorder-array-to-get-same-bst.cpp
@@ -3,7 +3,9 @@ public:
     int mod = 1e9+7;
     long long postorder(vector<int> &nums, vector<vector<long long>> &ncr) {
         int n = nums.size();
+        //no element then no rearrangement
         if(n == 0) return 1;
+        //left subtree elements and right subtree elements
         vector<int> left, right;
         for(int i = 1;i < n;i++) {
             if(nums[i] < nums[0]) left.push_back(nums[i]);
@@ -12,11 +14,13 @@ public:
         int r = left.size();
         long long left_arrange = postorder(left,ncr)%mod;
         long long right_arrange = postorder(right,ncr)%mod;
+        //formula
         long long res = ((ncr[n-1][r] * left_arrange)%mod * right_arrange)%mod;
         return res;
     }
     int numOfWays(vector<int>& nums) {
         int n = nums.size();
+        //calculate all possible ncr combinations using dp
         vector<vector<long long>> ncr(n+1,vector<long long> (n+1,1));
         for(int i = 1;i <= n;i++) {
             for(int j = 1;j < i;j++) {

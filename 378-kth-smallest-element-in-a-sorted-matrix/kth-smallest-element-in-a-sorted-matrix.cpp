d2049410647f1f1d338fb4060d36c1b1c0d4388d
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-09-17 13:39:24 +0530

    Time: 18 ms (88.68%) | Memory: 15.7 MB (96.60%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    //how to smallest elements count?
+    //we know that each row is sorted
+    //so in each row apply lower bound for that particular element
+    //store lesser count in total
     int getCount(vector<vector<int>> &matrix, int ele) {
         int n = matrix.size();
         int count = 0;
@@ -10,10 +14,13 @@ public:
     }
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n = matrix.size();
+        //apply lower bound on result
+        //starting element is min and last element in matrix is max
         int low = matrix[0][0],high = matrix[n-1][n-1];
         int ans = -1;
         while(low <= high) {
             int mid = low + (high - low)/2;
+            //store result and opt for better result in due process
             if(getCount(matrix,mid) >= k) {
                 ans = mid;
                 high = mid - 1;

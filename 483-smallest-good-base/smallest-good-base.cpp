b581a06e767fc2ee8090289ec8f3d5c42f21e24e
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-09-19 06:09:24 +0530

    Time: 5 ms (9.60%) | Memory: 7.5 MB (93.60%) - LeetSync

--- 483-smallest-good-base/smallest-good-base.cpp
+++ 483-smallest-good-base/smallest-good-base.cpp
@@ -3,7 +3,9 @@ public:
     string smallestGoodBase(string n) {
         typedef unsigned long long ll;
         ll num = stoll(n);
+        //greedily pick bit length
         for(int bit = 63;bit > 0;bit--) {
+            //check if sum equals num using binary search
             ll low = 2, high = num - 1;
             while(low <= high) {
                 ll mid = low + (high - low)/2;
@@ -12,13 +14,18 @@ public:
                 for(int len = 0;len <= bit;len++) {
                     sum += val;
                     if(sum >= num) break;
+                    //val*mid directly added to sum might generate overflow
+                    //for that check before hand
+                    //if num < sum + val*mid
+                    //this we have modified to below condition
+                    //to prevent overflow
                     if(len < bit && (num - sum)/mid <  val) {
                         cantadd = true;
                         break;
                     }
                     if(len < bit) val *= mid;
                 }
-                cout << mid << ' ' << sum << endl;
+                // cout << mid << ' ' << sum << endl;
                 if(sum > num || cantadd) {
                     high = mid - 1;
                 }

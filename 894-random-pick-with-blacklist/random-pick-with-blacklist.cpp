a5470c9fdf8e81dfaca5d28c2f64e35a45340aab
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-09-20 21:04:09 +0530

    Time: 101 ms (63.74%) | Memory: 78.3 MB (18.68%) - LeetSync

---
+++ 894-random-pick-with-blacklist/random-pick-with-blacklist.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+class Solution {
+public:
+    vector<int> nums;
+    int size;
+    Solution(int n, vector<int>& blacklist) {
+        set<int> se(blacklist.begin(),blacklist.end());
+        n = min(100000,n);
+        for(int i = 0;i < n;i++) {
+            if(!se.count(i)) nums.push_back(i); 
+        }
+        size = nums.size();
+    }
+
+    int pick() {
+        int ind = rand() % size;
+        return nums[ind];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(n, blacklist);
+ * int param_1 = obj->pick();
+ */

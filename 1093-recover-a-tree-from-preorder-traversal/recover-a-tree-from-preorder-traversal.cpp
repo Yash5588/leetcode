d9fc7328996eac18ecfcab7034695db26f59f5d2
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-09-20 22:12:26 +0530

    Time: 19 ms (31.91%) | Memory: 21 MB (42.32%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
@@ -14,18 +14,23 @@ public:
     TreeNode* root = nullptr;
     void preorder(int targetDepth, int node, TreeNode* root, int depth) {
         if(root) {
+            //check if we reach destination
             if(depth + 1 == targetDepth) {
+                //if left node not preset insert it at left
                 if(root->left == nullptr) {
                     root->left = new TreeNode(node);
                 }
+                //otherwise insert at right side
                 else {
                     root->right = new TreeNode(node);
                 }
                 return;
             }
+            //move to right if present
             if(root->right) {
                 preorder(targetDepth, node, root->right, depth + 1);
             }
+            //else move to left
             else {
                 preorder(targetDepth, node, root->left, depth + 1);
             }
@@ -33,12 +38,16 @@ public:
     }
     void buildTree(vector<pair<int,int>> pairs) {
         int n = pairs.size();
+        //build tree according to mentioned in question
         root = new TreeNode(pairs[0].second);
         for(int i = 1;i < n;i++) {
             preorder(pairs[i].first,pairs[i].second,root,0);
         }
     }
     TreeNode* recoverFromPreorder(string str) {
+        //form as pairs for easy access
+        //first -> depth
+        //second -> node val
         vector<pair<int,int>> pairs;
         int n = str.length(),i = 0;
         while(i < n) {

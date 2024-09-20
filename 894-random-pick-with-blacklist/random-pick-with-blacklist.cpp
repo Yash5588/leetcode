53de263ca57083e2ccb9dabc2677d7f32f3ea61c
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-09-20 23:42:53 +0530

    Time: 159 ms (5.49%) | Memory: 93.8 MB (5.49%) - LeetSync

--- 894-random-pick-with-blacklist/random-pick-with-blacklist.cpp
+++ 894-random-pick-with-blacklist/random-pick-with-blacklist.cpp
@@ -4,12 +4,8 @@ public:
     vector<int> nums;
     int size;
     Solution(int n, vector<int>& blacklist) {
-        //nlogn time 
-        //sorting and removing all the duplicates
         unordered_set<int> se(blacklist.begin(),blacklist.end());
-        //get all the elements in range n which are not present
-        //in nums
-        n = min(100000,n);
+        n = min(1000000,n);
         for(int i = 0;i < n;i++) {
             if(!se.count(i)) nums.push_back(i); 
         }
@@ -17,8 +13,6 @@ public:
     }
 
     int pick() {
-        //pick a random index and send it from nums 
-        //these are anyway not present in black box
         int ind = rand() % size;
         return nums[ind];
     }

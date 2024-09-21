df1e76052f52849f80b3a65ff8ce05cf7755e730
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-09-21 23:38:19 +0530

    Time: 1093 ms (19.02%) | Memory: 331.4 MB (5.52%) - LeetSync

--- 3001-apply-operations-to-maximize-score/apply-operations-to-maximize-score.cpp
+++ 3001-apply-operations-to-maximize-score/apply-operations-to-maximize-score.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int mod = 1e9 + 7;
+    //optimised power calculator O(logn)
     long long power(long long base, long long exp) {
         long long res = 1;
         while(exp > 0) {
@@ -13,6 +14,7 @@ public:
     int maximumScore(vector<int>& nums, int k) {
         int n = nums.size();
         int N = 1e5 + 1;
+        //sieve of evasthones for getting all primes count for each number
         vector<long long> pscore(N,0);
         for(int i = 2;i < N;i++) {
             if(pscore[i] > 0) continue;
@@ -20,6 +22,15 @@ public:
                 pscore[j]++;
             }
         }
+        //we use stack to determine the range on left and right side
+        //such that we can consider that range all subarray combinations
+        //for which the current element is the max result generator
+        //eliminate greater and equal values to right till nearest greater value to the right
+        //eliminate only greater values to the left 
+        //NOTE: don't eliminate equal values becoz eliminating we are considering it in our range
+        //we can't consider it becoz in problem they mentioned smallest index right
+        //if we include equal element then smallest element index goes to that element rather than for current element
+        //it simply means range distortion
         stack<pair<long long,long long>> sta1,sta2;
         vector<long long> left(n,0),right(n,0);
         for(int i = 0;i < n;i++) {
@@ -44,16 +55,18 @@ public:
         for(int i = 0;i < n;i++) {
             combine[i] = {nums[i],left[i],right[i]};
         }
-        // for(auto &x : nums) cout << pscore[x] << endl;
-        // cout << endl;
-        // for(auto &x : left) cout << x << endl;
-        // cout << endl;
-        // for(auto &x : right) cout << x << endl;
+        //we have all the required things now sort the array 
+        //we greedily pick the max element and it's range subarrays
         sort(combine.begin(),combine.end(),greater<vector<long long>>());
         long long res = 1;
         for(int i = 0;i < n;i++) {
             if(k <= 0) break;
+            //formula to calculate number of subarrays the current element
+            //can form in it's range
+            //cnt = 1 + leftCount + rightCount + leftCount*rightCount
             long long pow = min((long long)k, 1 + combine[i][1] + combine[i][2] + combine[i][1]*combine[i][2]);
+            //power to the current element that many subarrays we have included
+            //multiply with result
             res = (res * power(combine[i][0],pow)) % mod;
             k -= pow;
         }

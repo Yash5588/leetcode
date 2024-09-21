141df78faf5c37746c890a59c41d0c554bc58634
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-09-21 06:58:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (32.76%) - LeetSync

--- 809-preimage-size-of-factorial-zeroes-function/preimage-size-of-factorial-zeroes-function.cpp
+++ 809-preimage-size-of-factorial-zeroes-function/preimage-size-of-factorial-zeroes-function.cpp
@@ -3,6 +3,11 @@ public:
     long long countZeros(long long num) {
         long long count = 0;
         long long start = 5;
+        //to get all the 5's in the product of factorial
+        //becoz 5 always contributes for each zero
+        //number of 5's = zeros at the end
+        //now we have to be careful of 25,125,625...
+        //25 - two 5's 125 - three 5's so we add they extra if present inside factorial
         while(start <= num) {
             count += num/start;
             start *= 5;
@@ -19,6 +24,7 @@ public:
             if(zeros < k) low = mid + 1;
             else if(zeros > k) high = mid - 1;
             else {
+                //every time we will be having result as either 5 or 0
                 ans = 5;
                 break;
             }

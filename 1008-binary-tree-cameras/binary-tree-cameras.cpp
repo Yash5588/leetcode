d7e32ca77e0789aef1e459567d44c243ffa4fd56
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-02 10:12:12 +0530

    Time: 41 ms (5.68%) | Memory: 32.5 MB (5.20%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.cpp
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -16,28 +16,29 @@ public:
         return root->left == nullptr && root->right == nullptr;
     }
     long long solve(TreeNode* root, bool placeCam, bool parentCam) {
+        //if we get assign camera to null
+        //then that path is wrong
         if(root == nullptr) {
             if(placeCam) return INT_MAX;
             return 0;
         }
-        if(isLeaf(root)) {
-            if(placeCam) return 1;
-            else {
-                if(parentCam) return 0;
-                return INT_MAX;
-            }
-        }
         if(dp.find({root,{placeCam,parentCam}}) != dp.end()) {
             return dp[{root,{placeCam,parentCam}}];
         }
+        //root is safe by either it's parent has camera
+        //or root has camera 
         if(placeCam || parentCam) {
             long long left = min(solve(root->left,0,placeCam),solve(root->left,1,placeCam));
             long long right = min(solve(root->right,0,placeCam),solve(root->right,1,placeCam));
             if(placeCam) return dp[{root,{placeCam,parentCam}}] = 1 + left + right;
             return  dp[{root,{placeCam,parentCam}}] = left + right;
         }
+        //this case it when root parent doesn't have camera 
+        //and root also doesn't have camera
         else {
             long long placeLeft,placeRight;
+            //we forcefully assign camera to one child
+            //and give choice to another
             placeLeft = solve(root->left,1,placeCam) + min(solve(root->right,1,placeCam),solve(root->right,0,placeCam));
             placeRight = solve(root->right,1,placeCam) + min(solve(root->left,1,placeCam),solve(root->left,0,placeCam));
             return  dp[{root,{placeCam,parentCam}}] = min(placeLeft,placeRight);

feab61b2974519fec48c6f0c943fa93706fea03d
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-05 18:34:06 +0530

    Time: 20 ms (35.91%) | Memory: 19.9 MB (15.59%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isCycle(int node, vector<vector<int>> &adj, vector<bool> &visited, vector<bool> &path_visited) {
+        visited[node] = true;
+        path_visited[node] = true;
+        for(auto &child : adj[node]) {
+            if(!visited[child]) {
+                if(isCycle(child, adj, visited, path_visited)) return true;
+            }
+            else if(path_visited[child]) {
+                return true;
+            }
+        }
+        path_visited[node] = false;
+        return false;
+    }
+    bool canFinish(int V, vector<vector<int>>& pre) {
+        vector<vector<int>> adj(V, vector<int>());
+        for(auto &x : pre) {
+            adj[x[1]].push_back(x[0]);
+        }
+        vector<bool> visited(V,false);
+        vector<bool> path_visited(V,false);
+        for(int i = 0;i < V;i++) {
+            if(!visited[i]) {
+                if(isCycle(i, adj, visited, path_visited)) return false;
+            }
+        }
+        return true;
+    }
+};

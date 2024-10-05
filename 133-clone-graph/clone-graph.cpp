b16e5dc21704bf369e404eb9d55a0e82dd106472
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-05 15:10:01 +0530

    Time: 7 ms (23.17%) | Memory: 11.9 MB (35.92%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -23,13 +23,19 @@ class Solution {
 public:
     void dfs(Node* new_node, Node* node, unordered_map<Node*,Node*> &visited) {
         if(node == nullptr) return;
+        //check if the clone of that node exists
         if(visited[node] != nullptr) return;
+        //if not assign
         visited[node] = new_node;
         for(auto &child : node->neighbors) {
+            //check if already clone exists for neighbors
             if(visited[child] != nullptr) {
+                //if exists then assign it's address
                 new_node->neighbors.push_back(visited[child]);
+                //it also indicates that this node is already visited
             }
             else {
+                //otherwise create a new node
                 Node* new_child = new Node(child->val, {});
                 new_node->neighbors.push_back(new_child);
                 dfs(new_child, child, visited);
@@ -38,8 +44,10 @@ public:
     }
 
     Node* cloneGraph(Node* node) {
+        //store original and clone node
         unordered_map<Node*,Node*> visited;
         if(node == nullptr) return nullptr;
+        //initially create node
         Node* new_node = new Node(node->val, {});
         dfs(new_node,node,visited);
         return new_node;

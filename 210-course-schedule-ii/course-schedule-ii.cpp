22f57d1941b8a15901c3dbd1de6337f217703eda
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-05 19:20:33 +0530

    Time: 8 ms (96.61%) | Memory: 18.9 MB (16.05%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
+    bool isCycle(int node, vector<vector<int>> &adj, vector<bool> &visited, vector<bool> &path_visited, stack<int> &sta) {
+        visited[node] = true;
+        path_visited[node] = true;
+        for(auto &child : adj[node]) {
+            if(!visited[child]) {
+                if(isCycle(child, adj, visited, path_visited, sta)) {
+                    return true;
+                }
+            }
+            else if(path_visited[child]) return true;
+        }
+        path_visited[node] = false;
+        sta.push(node);
+        return false;
+    }
     vector<int> findOrder(int V, vector<vector<int>>& pre) {
         vector<vector<int>> adj(V, vector<int> ());
         for(auto &x : pre) {
             adj[x[1]].push_back(x[0]);
         }
+        stack<int> sta;
+        vector<bool> visited(V,false);
+        vector<bool> path_visited(V,false);
+        for(int i = 0;i < V;i++) {
+            if(!visited[i]) {
+                if(isCycle(i, adj, visited, path_visited, sta)) {
+                    return {};
+                }
+            }
+        }
         vector<int> order;
-        queue<int> que;
-        vector<int> indegree(V,0);
-        for(int i = 0;i < V;i++) {
-            for(auto &x : adj[i]) {
-                indegree[x]++;
-            }
+        while(!sta.empty()) {
+            order.push_back(sta.top());
+            sta.pop();
         }
-        for(int i = 0;i < V;i++) {
-            if(indegree[i] == 0) que.push(i);
-        }
-        while(!que.empty()) {
-            int curr = que.front();
-            order.push_back(curr);
-            que.pop();
-            for(auto &x : adj[curr]) {
-                indegree[x]--;
-                if(indegree[x] == 0)  que.push(x);
-            }
-        }
-        if(order.size() == V) return order;
-        return {};
+        return order;
     }
 };

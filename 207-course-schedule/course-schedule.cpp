94549dc38138b158a19da6ba955bc19656008cb4
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-05 18:35:16 +0530

    Time: 14 ms (79.78%) | Memory: 19.9 MB (14.67%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -15,12 +15,14 @@ public:
         return false;
     }
     bool canFinish(int V, vector<vector<int>>& pre) {
+        //graph conversion
         vector<vector<int>> adj(V, vector<int>());
         for(auto &x : pre) {
             adj[x[1]].push_back(x[0]);
         }
         vector<bool> visited(V,false);
         vector<bool> path_visited(V,false);
+        //code to detect cycle in directed graph
         for(int i = 0;i < V;i++) {
             if(!visited[i]) {
                 if(isCycle(i, adj, visited, path_visited)) return false;

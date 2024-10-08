bd6033dfddd473b6b2a200d17b1180ce7fe36019
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-08 11:47:06 +0530

    Time: 11 ms (9.36%) | Memory: 15.1 MB (5.89%) - LeetSync

--- 770-couples-holding-hands/couples-holding-hands.cpp
+++ 770-couples-holding-hands/couples-holding-hands.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-    int minSwapsCouples(vector<int>& row) {
-        int n = row.size();
-        vector<int> indices(n,0);
-        for(int i = 0;i < n;i++) {
-            indices[row[i]] = i;
-        }
+    int dfs(int node, unordered_map<int, vector<int>> adj, vector<bool> &visited) {
+        visited[node] = true;
         int cnt = 0;
-        for(int i = 0;i < n;i += 2) {
-            if(row[i] & 1) {
-                if(row[i+1] != row[i] - 1) {
-                    int first = row[i+1];
-                    int second = row[i]-1;
-                    int temp = indices[first];
-                    indices[first] = indices[second];
-                    indices[second] = temp;
-                    row[indices[first]] = first;
-                    row[indices[second]] = second;
-                    cnt++;
-                }
+        for(auto &child : adj[node]) {
+            if(!visited[child]) {
+                cnt += 1 + dfs(child, adj, visited);
             }
-            else {
-                if(row[i+1] != row[i] + 1) {
-                    int first = row[i+1];
-                    int second = row[i]+1;
-                    int temp = indices[first];
-                    indices[first] = indices[second];
-                    indices[second] = temp;
-                    row[indices[first]] = first;
-                    row[indices[second]] = second;
-                    cnt++;
-                }
-            }
-            // for(auto &x : row) cout << x << endl;
-            // cout << endl;
         }
         return cnt;
+    }
+    int minSwapsCouples(vector<int>& row) {
+        int n = row.size();
+        unordered_map<int,vector<int>> adj;
+        for(int i = 0;i < n;i += 2) {
+            adj[row[i]/2].push_back(row[i+1]/2);
+            adj[row[i+1]/2].push_back(row[i]/2);
+        }
+        vector<bool> visited(n/2,false);
+        int ans = 0;
+        for(int i = 0;i < n/2;i++) {
+            if(!visited[i]) {
+                int cnt = dfs(i, adj, visited);
+                ans += cnt;
+            }
+        }
+        return ans;
     }
 };

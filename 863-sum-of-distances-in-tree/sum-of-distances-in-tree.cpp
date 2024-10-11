0b86a340d5fac332d5acf002015b7dddba86a6c5
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-11 07:46:54 +0530

    Time: 772 ms (5.06%) | Memory: 127.1 MB (8.52%) - LeetSync

---
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>> adj;
+    unordered_map<int,int> children;
+    int getChildren(int root, int parent) {
+        int cnt = 0;
+        for(auto &child : adj[root]) {
+            if(child != parent) cnt += getChildren(child,root);
+        }
+        children[root] = cnt + 1;
+        return cnt + 1;
+    }
+
+    int getScore(int root) {
+        int score = 0;
+        queue<pair<int,int>> que;
+        que.push({0,-1});
+        int k = 0;
+        while(!que.empty()) {
+            int size = que.size();
+            score += k*size;
+            while(size--) {
+                int node = que.front().first;
+                int parent = que.front().second;
+                que.pop();
+                for(auto &child : adj[node]) {
+                    if(child != parent) {
+                        que.push({child,node});
+                    }
+                }
+            }
+            k++;
+        }
+        return score;
+    }
+
+    void assignScore(int root, int parent, vector<int> &res) {
+        int n = res.size();
+        res[root] = res[parent] - children[root] + (n - children[root]);
+        for(auto &child : adj[root]) {
+            if(child != parent) assignScore(child, root, res);
+        } 
+    }
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        for(auto &x : edges) {
+            adj[x[0]].push_back(x[1]);
+            adj[x[1]].push_back(x[0]);
+        }
+        children[0] = getChildren(0,-1);
+        int root_score = getScore(0);
+        vector<int> res(n,0);
+        res[0] = root_score;
+        for(auto &child : adj[0]) {
+            assignScore(child,0,res);
+        }
+        // for(int i = 1;i < n;i++) {
+        //     res[i] = res[node_parent[i]] - children[i] + (n - children[i]);
+            // cout <<  i << ' ' << score[node_parent[i]] << ' ' << res[i] << endl;
+        // }
+
+        // for(auto &x : node_parent) {
+        //     cout << x.first << ' ' << x.second << endl;
+        // }
+        return res;
+    }
+};

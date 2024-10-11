842498733fdc2d84a523c83d4dfecdd995b33db7
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-12 00:50:41 +0530

    Time: 121 ms (62.76%) | Memory: 52.8 MB (62.01%) - LeetSync

---
+++ 918-reachable-nodes-in-subdivided-graph/reachable-nodes-in-subdivided-graph.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    unordered_map<int, vector<pair<int,int>>> adj;
+    unordered_map<int,bool> visited;
+    map<pair<int,int>, int> left;
+    // int dfs(int node, int moves) {
+    //     visited[node] = true;
+    //     int cnt = 0;
+    //     for(auto &it : adj[node]) {
+    //         int child = it.first;
+    //         int dist = it.second;
+    //         if(!visited[child]) {
+    //             if(moves >= dist) {
+    //                 cnt += dist + dfs(child, moves - dist);
+    //             }
+    //             else {
+    //                 cnt += moves;
+    //                 left[{child,node}] = dist - moves - 1;
+    //             }
+    //         }
+    //         else {
+    //             cnt += min(moves, left[{node,child}]);
+    //         }
+    //         // if(node == 0) cout << child << ' ' << cnt << endl;
+    //     }
+    //     return cnt;
+    // }
+    int reachableNodes(vector<vector<int>>& edges, int maxMoves, int n) {
+        for(auto &x : edges) {
+            adj[x[0]].push_back({x[1], x[2]+1});
+            adj[x[1]].push_back({x[0], x[2]+1});
+        }
+        vector<int> distance(n,INT_MAX);
+        distance[0] = 0;
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> que;
+        que.push({0,0});
+        while(!que.empty()) {
+            int dis = que.top()[0];
+            int node = que.top()[1];
+            que.pop();
+            for(auto &it : adj[node]) {
+                int dest_node = it.first;
+                int edgeWeight = it.second;
+                if(dis + edgeWeight < distance[dest_node]) {
+                    distance[dest_node] = dis + edgeWeight;
+                    que.push({distance[dest_node],dest_node});
+                }
+            }
+        }
+        int ans = 0;
+        for(auto &x : distance) {
+            if(x <= maxMoves) ans++; 
+        }
+        for(auto &x : edges) {
+            int u = x[0];
+            int v = x[1];
+            int subnodes = x[2];
+            int a = (distance[u] > maxMoves) ? 0 : min(maxMoves - distance[u], subnodes);
+            int b = (distance[v] > maxMoves) ? 0 : min(maxMoves - distance[v], subnodes);
+            int totalCoveredSubNodes = min(a+b, subnodes);
+            ans += totalCoveredSubNodes;
+        }
+        return ans;
+    }
+};

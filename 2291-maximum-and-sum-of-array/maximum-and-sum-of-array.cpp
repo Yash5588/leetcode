d7f828fd3eb44a4b5b4a0ba5b2d33c0bdb16ce96
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-15 21:21:31 +0530

    Time: 405 ms (29.77%) | Memory: 92.5 MB (18.32%) - LeetSync

--- 2291-maximum-and-sum-of-array/maximum-and-sum-of-array.cpp
+++ 2291-maximum-and-sum-of-array/maximum-and-sum-of-array.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    int dp[20][1<<18];
-    int solve(vector<int> &nums, int pos, int mask, int slots) {
+    int dp[18][1<<10][1<<10];
+    int maxm(vector<int>& nums, int mask1,int mask2, int idx1, int slots) {
         int n = nums.size();
-        if(pos >= n) return 0;
-        if(dp[pos][mask] != -1) return dp[pos][mask];
-        int ma = INT_MIN;
-        for(int i = 1;i < slots;i++) {
-            int bit = 2*i-1,next_bit = 2*i-2;
-            if((mask & (1 << bit)) == 0) {
-                ma = max(ma,(nums[pos] & i) + solve(nums, pos+1,mask | (1<<bit),slots));
-            }
-            else if((mask & (1 << next_bit)) == 0) {
-                ma = max(ma,(nums[pos] & i) + solve(nums, pos+1,mask | (1<<next_bit),slots));
-            }
+        if(idx1>=n) return 0;
+        if(dp[idx1][mask1][mask2]!= -1) return dp[idx1][mask1][mask2];
+        int mxm=INT_MIN;
+        for(int i=0; i<slots; i++) {
+            if((mask1 & (1 << i)) == 0) mxm =  max(mxm, (nums[idx1]&i)+maxm(nums,mask1 | (1 << i),mask2,idx1+1,slots));
+            else if((mask2 & (1 << i)) == 0) mxm = max(mxm, (nums[idx1]&i)+maxm(nums,mask1,mask2|(1<<i),idx1+1,slots));
         }
-        return dp[pos][mask] = ma;
+        return dp[idx1][mask1][mask2] = mxm;
     }
     int maximumANDSum(vector<int>& nums, int numSlots) {
-        int n = nums.size();
-        // vector<int> slots(numSlots+1,0);
         memset(dp,-1,sizeof(dp));
-        return solve(nums,0,0,numSlots+1);
+        int n = nums.size();
+        return maxm(nums,0,0,0, numSlots+1);
     }
 };

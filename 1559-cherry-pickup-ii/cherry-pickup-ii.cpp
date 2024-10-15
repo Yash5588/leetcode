cc1be90a755a53903ef45ed460fdbaec339a5200
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-15 22:44:13 +0530

    Time: 352 ms (5.10%) | Memory: 116.7 MB (5.00%) - LeetSync

---
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[70][70][70][70];
+    int solve(vector<vector<int>> &grid, int row1, int col1, int row2, int col2) {
+        int n = grid.size();
+        int m = grid[0].size();
+        if(row1 >= n || row2 >= n || col1 >= m || col2 >= m) return INT_MIN;
+        if(col1 < 0 || col2 < 0) return INT_MIN;
+        if(row1 == row2 && col1 == col2) return INT_MIN;
+        if(row1 == n-1 && row2 == n-1) {
+            return grid[row1][col1] + grid[row2][col2];
+        }
+        if(dp[row1][col1][row2][col2] != -1) return dp[row1][col1][row2][col2];
+        int ma = INT_MIN;
+        for(int first = -1;first <= 1;first++) {
+            for(int second = -1;second <= 1;second++) {
+                ma = max(ma, grid[row1][col1] + grid[row2][col2] + solve(grid, row1+1,col1+first,row2+1,col2+second));
+            }
+        }
+        return dp[row1][col1][row2][col2] = ma;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        memset(dp,-1,sizeof(dp));
+        return solve(grid,0,0,0,m-1);
+    }
+};

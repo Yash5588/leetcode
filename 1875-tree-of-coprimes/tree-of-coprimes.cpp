e9fa72ca09211e15d3426483da6d534a2c99c554
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-25 05:11:08 +0530

    Time: 835 ms (67.62%) | Memory: 219.7 MB (10.95%) - LeetSync

--- 1875-tree-of-coprimes/tree-of-coprimes.cpp
+++ 1875-tree-of-coprimes/tree-of-coprimes.cpp
@@ -5,8 +5,12 @@ public:
     vector<int> coprimes;
     vector<int> res;
     void solve(vector<int> &nums, int node, int par) {
+        //update the depth of each node
         depth[node] = depth[par] + 1;
+        //store the original nums value
         int prev = coprimes[nums[node]];
+        //change the coprimes[nums[i]] = i which is current node
+        //in that particular subtree
         coprimes[nums[node]] = node;
         // cout << node << endl;
         for(auto &child : adj[node]) {
@@ -14,10 +18,15 @@ public:
             int ma = INT_MIN;
             for(int i = 1;i <= 50;i++) {
                 // cout << i << ' ' << coprimes[i] << "   ";
+                //get all the latest ancestors for each value
                 int ances = coprimes[i];
                 if(ances != -1) {
+                    //check if ancestor exists or not
                     if(__gcd(nums[child],nums[ances]) == 1) {
+                        //if exists then check gcd
                         if(ma < depth[ances]) {
+                            //if ancestor exists and gcd 1 compare the depth
+                            //we need the ancestor with max depth
                             ma = depth[ances];
                             res[child] = ances;
                         }
@@ -27,6 +36,10 @@ public:
             // cout << endl;
             solve(nums,child,node);
         }
+        //after that subtree is completed we are backtracking 
+        //so revert back to original value 
+        //we are only interested in nums values of ancestors so we
+        //have to backtrack after completing all children
         coprimes[nums[node]] = prev;
     }
     vector<int> getCoprimes(vector<int>& nums, vector<vector<int>>& edges) {
@@ -37,6 +50,8 @@ public:
             adj[edge[1]].push_back(edge[0]);
         }
         res.resize(n,-1);
+        //coprimes to store the latest node with that value,
+        //anyway it's depth is the maximum in dfs
         coprimes.resize(51,-1);
         solve(nums,0,-1);
         return res;

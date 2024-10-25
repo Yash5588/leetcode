45d0e128e1df584ff9fe936d67d760be41ae1ee3
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-25 23:44:02 +0530

    Time: 1803 ms (5.22%) | Memory: 520.3 MB (5.22%) - LeetSync

--- 1645-find-a-value-of-a-mysterious-function-closest-to-target/find-a-value-of-a-mysterious-function-closest-to-target.cpp
+++ 1645-find-a-value-of-a-mysterious-function-closest-to-target/find-a-value-of-a-mysterious-function-closest-to-target.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int closestToTarget(vector<int>& arr, int target) {
         int n = arr.size();
-        vector<set<int>> sets(n+1);
+        vector<unordered_set<int>> sets(n+1);
         int ans = INT_MAX;
         for(int i = 0;i < n;i++) {
-            set<int> prev = sets[i];
-            set<int> curr = sets[i+1];
+            unordered_set<int> prev = sets[i];
+            unordered_set<int> curr = sets[i+1];
             curr.insert(arr[i]);
             for(auto &x : prev) {
                 curr.insert(x & arr[i]);

220779399b076a6bba37fdeb0aa1faab1e83e359
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-25 06:27:33 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (69.64%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,25 +1,55 @@
+class DisjointSet{
+public:
+    vector<int> rank, parent;
+    DisjointSet(int n) {
+        rank.resize(n+1,0);
+        parent.resize(n+1,0);
+        for(int i = 0;i <= n;i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node]) return node;
+        //path compression
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        //both nodes belong to same component
+        if(ulp_u == ulp_v) return;
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if(rank[ulp_u] > rank[ulp_v]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+};
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>> &adj, vector<bool> &visited) {
-        visited[node] = true;
-        for(int i = 0;i < adj[node].size();i++) {
-            if(adj[node][i] == 1) {
-                if(!visited[i]) {
-                    dfs(i, adj, visited);
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        DisjointSet ds(n);
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < n;j++) {
+                if(isConnected[i][j] == 1) {
+                    ds.unionByRank(i,j);
                 }
             }
         }
-    }
-    int findCircleNum(vector<vector<int>>& connect) {
-        int V = connect.size();
-        vector<bool> visited(V,false);
-        int ans = 0;
-        for(int i = 0;i < V;i++) {
-            if(!visited[i]) {
-                ans++;
-                dfs(i, connect, visited);
-            }
+        vector<int> parent = ds.parent;
+        int cnt = 0;
+        for(int i = 0;i < n;i++) {
+            if(i == parent[i]) cnt++;
         }
-        return ans;
+        return cnt;
     }
 };

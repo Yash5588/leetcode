c2ab9e966cbcfedcfb9b73cd8312ebcc47c600ab
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-25 23:25:47 +0530

    Time: 1467 ms (5.22%) | Memory: 455.1 MB (5.22%) - LeetSync

---
+++ 1645-find-a-value-of-a-mysterious-function-closest-to-target/find-a-value-of-a-mysterious-function-closest-to-target.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int closestToTarget(vector<int>& arr, int target) {
+        int n = arr.size();
+        vector<set<int>> sets(n+1);
+        int ans = INT_MAX;
+        for(int i = 0;i < n;i++) {
+            set<int> prev = sets[i];
+            set<int> curr = sets[i+1];
+            curr.insert(arr[i]);
+            for(auto &x : prev) {
+                curr.insert(x & arr[i]);
+            }
+            for(auto &x : curr) {
+                ans = min(ans, abs(x-target));
+            }
+            sets[i] = prev;
+            sets[i+1] = curr;
+        }
+        return ans;
+    }
+};

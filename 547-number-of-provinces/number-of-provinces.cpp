4b1458a2008e597ce875f0bdc3124054ef71b377
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-25 06:32:53 +0530

    Time: 1 ms (96.48%) | Memory: 18.5 MB (69.64%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -4,28 +4,36 @@ public:
     DisjointSet(int n) {
         rank.resize(n+1,0);
         parent.resize(n+1,0);
+        //initially the parents are themselves
         for(int i = 0;i <= n;i++) {
             parent[i] = i;
         }
     }
 
+    //to find the ultimate parent of a given node
     int findUPar(int node) {
         if(node == parent[node]) return node;
         //path compression
         return parent[node] = findUPar(parent[node]);
     }
 
+    //to merge the both components based on rank
     void unionByRank(int u, int v) {
+        //get the ultimate parents of both nodes
+        //path compression is done on the way
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
         //both nodes belong to same component
         if(ulp_u == ulp_v) return;
+        //if rank is not same for ultimate parents
         if(rank[ulp_u] < rank[ulp_v]) {
             parent[ulp_u] = ulp_v;
         }
         else if(rank[ulp_u] > rank[ulp_v]) {
             parent[ulp_v] = ulp_u;
         }
+        //if rank is same then merge anyone of our choice
+        //then update the rank in this case.
         else {
             parent[ulp_v] = ulp_u;
             rank[ulp_u]++;

65def1f922d43e054b4ad67427b659a1efe73b37
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-10-31 16:49:37 +0530

    Time: 244 ms (5.26%) | Memory: 9.5 MB (51.99%) - LeetSync

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
+    long long dp[51][101][51];
     int mod = 1e9+7;
-    long long dp[51][51][101];
-    long long solve(int n, int pos, int m, int k, int prev) {
-        if(pos >= n) {
-            if(k == 0) return 1;
+    long long arrays(int idx, int mxm, int cost, int n, int m, int k) {
+        if(idx==n) {
+            if(cost==k) return 1;
             return 0;
         }
-        if(dp[pos][k][prev] != -1) return dp[pos][k][prev];
-        long long cnt = 0;
-        for(int i = 1;i <= m;i++) {
-            if(prev < i) {
-                if(k > 0) {
-                    cnt = (cnt + solve(n,pos+1,m,k-1,i)%mod)%mod;
-                }
+        if(dp[idx][mxm][cost]!=-1) return dp[idx][mxm][cost];
+        long long count=0;
+        for(int i=1; i<=m; i++) {
+            if(i>mxm) {
+                count = ((count%mod) + (arrays(idx+1, i, cost+1, n, m, k)%mod))%mod;
             }
-            else cnt =  (cnt + solve(n,pos+1,m,k,prev)%mod)%mod;
+            else {
+                count = ((count%mod) + (arrays(idx+1, mxm, cost, n, m, k)%mod))%mod;
+            }
+            count = count%mod;
         }
-        return dp[pos][k][prev] = cnt;
+        return dp[idx][mxm][cost] = count;
     }
     int numOfArrays(int n, int m, int k) {
         memset(dp,-1,sizeof(dp));
-        return solve(n,0,m,k,0)%mod;
+        return arrays(0,0,0,n, m,k)%mod;
     }
 };

dcd99d960828a971788fd6e0dfb96758ca5d904e
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-11-06 22:11:38 +0530

    Time: 1110 ms (40.34%) | Memory: 339.9 MB (21.36%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
@@ -6,11 +6,16 @@ public:
 
          for(int i=0; i<n; i++){
              unordered_set<int>curr;
+             //curr is for only ith index not for whole array
+             //check till i-1 and get all possible XORS
              for(auto x:prev){
                  curr.insert(x|arr[i]);
              }
+             //insert curr element as well
              curr.insert(arr[i]);
+             //later insert all unique xors in result to get all unique xors
              for(auto &x : curr) ans.insert(x);
+             //modify prev with curr
              prev = curr;
          }
         return ans.size();

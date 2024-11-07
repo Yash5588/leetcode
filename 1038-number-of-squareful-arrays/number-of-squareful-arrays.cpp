8c5bf03dc722449fbfde3c8e3e710d85fc2a5f8e
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-11-08 00:01:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (53.20%) - LeetSync

--- 1038-number-of-squareful-arrays/number-of-squareful-arrays.cpp
+++ 1038-number-of-squareful-arrays/number-of-squareful-arrays.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+    int dp[12][1<<13];
     int solve(vector<int> &nums, int prevInd, int mask) {
         int n = nums.size();
         if(mask == (1 << n)-1) {
             return 1;
         }
+        if(dp[prevInd][mask] != -1) return dp[prevInd][mask];
         int cnt = 0;
         for(int i = 0;i < n;i++) {
             if(mask & (1 << i)) continue;
@@ -13,12 +15,13 @@ public:
                 cnt += solve(nums, i, mask | (1 << i));
             }
         }
-        return cnt;
+        return dp[prevInd][mask] = cnt;
     }
     int numSquarefulPerms(vector<int>& nums) {
         int n = nums.size();
         int cnt = 0;
         sort(nums.begin(),nums.end());
+        memset(dp,-1,sizeof(dp));
         cnt += solve(nums,0,1<<0);
         for(int i = 1;i < n;i++) {
             if(nums[i] != nums[i-1]) {

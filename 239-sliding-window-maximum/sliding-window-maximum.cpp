ed7b0cd0d369dbe068b77db7d3f7cf49e8245ff6
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-11-10 22:33:15 +0530

    Time: 418 ms (5.04%) | Memory: 214.7 MB (5.71%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -9,6 +9,8 @@ public:
         }
         res.push_back(*se.begin());
         for(int i = 0;i < n-k;i++) {
+            //se.erase() removes all occurances
+            //to remove single occurance we use find which returns the iterator
             se.erase(se.find(nums[i]));
             se.insert(nums[i+k]);
             res.push_back(*se.begin());

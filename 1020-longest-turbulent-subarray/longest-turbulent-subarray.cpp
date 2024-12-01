f81f7434407ec1bc4fc20cca2f2929450a44f060
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-12-01 07:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (26.85%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
@@ -4,19 +4,29 @@ public:
         int n = arr.size();
         if(n == 1) return 1;
         bool greater;
+        //initialize opposite of parity
+        //for greater initialize with false so that if it is greater case when occurs
+        //we can assume that previous pair is SMALLER so we can continue it is like base
+        //case
         if(arr[0] > arr[1]) greater = false;
         else greater = true;
         int first = 0;
         int ma = INT_MIN;
         for(int last = 1;last < n;last++) {
+            //move first pointer when we can't form turbulent subarray
             if(arr[last-1] > arr[last]) {
+                //prev pair is greater and current pair is also greater so it is not turbulent subarray
                 if(greater == true) first = last - 1;
                 greater = true;
             }
+            //greater and condition should not have same parity
             else if(arr[last-1] < arr[last]) {
                 if(greater == false) first = last - 1;
                 greater = false;
             }
+            //equal to case is handled specially
+            //we can't include = pair in our new turbulent subarray
+            //so we ignore it so move it to right ignore (right-1) pair
             else {
                 first = last;
                 if(last+1 < n) {

16b7c60268d997b259361e4914ed172608388e72
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-12-06 16:20:50 +0530

    Time: 367 ms (8.74%) | Memory: 212.4 MB (6.45%) - LeetSync

--- 1286-constrained-subsequence-sum/constrained-subsequence-sum.cpp
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     //top down approach O(n^2)
-    unordered_map<string,int> dp;
-    long long solve(vector<int> &nums, int k, int prev, int pos, int flag) {
-        long long n = nums.size();
-        if(pos >= n) {
-            if(flag == 0) return INT_MIN;
-            return 0;
-        }
-        string key = to_string(prev) + '#' + to_string(pos) + to_string(flag);
-        if(dp.find(key) != dp.end()) return dp[key];
-        long long pick = INT_MIN,not_pick = INT_MIN;
-        if(prev == -1 || pos - prev <= k) {
-            pick = nums[pos] + solve(nums,k,pos,pos+1,1);
-        }
-        not_pick = solve(nums,k,prev,pos+1,flag);
-        return dp[key] = max(pick,not_pick);
-    }
+    // unordered_map<string,int> dp;
+    // long long solve(vector<int> &nums, int k, int prev, int pos, int flag) {
+    //     long long n = nums.size();
+    //     if(pos >= n) {
+    //         if(flag == 0) return INT_MIN;
+    //         return 0;
+    //     }
+    //     string key = to_string(prev) + '#' + to_string(pos) + to_string(flag);
+    //     if(dp.find(key) != dp.end()) return dp[key];
+    //     long long pick = INT_MIN,not_pick = INT_MIN;
+    //     if(prev == -1 || pos - prev <= k) {
+    //         pick = nums[pos] + solve(nums,k,pos,pos+1,1);
+    //     }
+    //     not_pick = solve(nums,k,prev,pos+1,flag);
+    //     return dp[key] = max(pick,not_pick);
+    // }
     int constrainedSubsetSum(vector<int>& nums, int k) {
         //bottom up approach
         int n = nums.size();
@@ -27,6 +27,7 @@ public:
         dp[0] = nums[0];
         for(int i = 1;i < n;i++) {
             //no need to remove if sliding window size is less than k
+            //window exceeds then remove those elements beyond window size
             if(i > k) {
                 se.erase(se.find(dp[i-k-1]));
             }

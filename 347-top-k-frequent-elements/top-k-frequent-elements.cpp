bb2f7b0413d21cc28b5555c96114517b9e400d01
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-12-09 22:18:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (7.86%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -3,25 +3,20 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n = nums.size();
         unordered_map<int,int> count;
-        multiset<pair<int,int>,greater<pair<int,int>>> se;
-        for(int i = 0;i < n;i++) {
-            count[nums[i]]++;
-            if(count[nums[i]] == 1) {
-                se.insert({1,nums[i]});
-            }
-            //greater than 1
-            else {
-                int cnt = count[nums[i]]-1;
-                se.erase(se.find({cnt,nums[i]}));
-                se.insert({cnt+1,nums[i]});
-            }
+        vector<vector<int>> bucket(n+1);
+        for(auto &x : nums) count[x]++;
+        for(auto &[key,cnt] : count) {
+            bucket[cnt].push_back(key);
         }
         vector<int> res;
-        int c = 0;
-        for(auto &x : se) {
-            if(c >= k) break;
-            res.push_back(x.second);
-            c++;
+        int cnt = 0;
+        for(int i = n;i >= 0;i--) {
+            for(auto &x : bucket[i]) {
+                res.push_back(x);
+                cnt++;
+                if(cnt >= k) break;
+            }
+            if(cnt >= k) break;
         }
         return res;
     }

2b151774c3782775d6792684687ccafc01c35ffa
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-12-13 09:58:41 +0530

    Time: 74 ms (90.19%) | Memory: 101.7 MB (76.02%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -1,51 +1,44 @@
 class Solution {
 public:
-    void merge(vector<pair<int,int>> &arr, int low, int mid, int high, vector<int> &count) {
-        int i = low, j = mid+1;
-        vector<pair<int,int>> temp;
-        while(i <= mid && j <= high) {
-            if(arr[i].first <= arr[j].first) {
-                temp.push_back(arr[j]);
-                j++;
-            }
-            else {
-                //when we are fixing the left element position
-                //in right subarray the remaining elements from j -> high are smaller than it
-                //also are in right side so we add them
-                count[arr[i].second] += (high - j + 1);
-                temp.push_back(arr[i]);
-                i++;
-            }
+    vector<int> bit;
+    int size;
+
+    int query(int id) {
+        int sum = 0;
+        while(id > 0) {
+            sum += bit[id];
+            id -= (id & -id);
         }
-        while(i <= mid) {
-            temp.push_back(arr[i]);
-            i++;
-        }
-        while(j <= high) {
-            temp.push_back(arr[j]);
-            j++;
-        }
-        for(int i = low;i <= high;i++) {
-            arr[i] = temp[i-low];
+        return sum;
+    }
+
+    void update(int id, int val) {
+        while(id <= size) {
+            bit[id] += val;
+            id += (id & -id);
         }
     }
-    void mergeSort(vector<pair<int,int>> &arr, int low, int high, vector<int> &count) {
-        if(low >= high) return;
-        int mid = low + (high - low)/2;
-        mergeSort(arr,low,mid,count);
-        mergeSort(arr,mid+1,high,count);
-        merge(arr,low,mid,high,count);
-    }
+
     vector<int> countSmaller(vector<int>& nums) {
-        //basically this is counting inversion technique
-        //of merge sort
         int n = nums.size();
-        vector<int> count(n,0);
-        vector<pair<int,int>> arr;
+        size = n;
+        bit.resize(n+1);
+        vector<pair<int,int>> ind_pairs;
         for(int i = 0;i < n;i++) {
-            arr.push_back({nums[i],i});
+            ind_pairs.push_back({nums[i],i});
         }
-        mergeSort(arr,0,n-1,count);
-        return count;
+        sort(ind_pairs.begin(),ind_pairs.end());
+        //populate bit array
+        for(int i = 0;i < n;i++) {
+            update(i+1, 0);
+        }
+        vector<int> res(n);
+        for(auto &x : ind_pairs) {
+            int idx = x.second;
+            //1 - based query(r) - query(l-1)
+            res[idx] = query(n) - query(idx);
+            update(idx+1, 1);
+        }
+        return res;
     }
 };

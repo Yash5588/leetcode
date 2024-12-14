0a77232393337dbb99fb0913f9bee6247dd89108
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-12-14 14:18:39 +0530

    Time: 7 ms (95.90%) | Memory: 15.2 MB (21.09%) - LeetSync

--- 126-word-ladder-ii/word-ladder-ii.cpp
+++ 126-word-ladder-ii/word-ladder-ii.cpp
@@ -7,8 +7,6 @@ public:
             //we will get res in reverse so reverse it
             reverse(temp.begin(),temp.end());
             res.push_back(temp);
-            //modifying original sequence so re reverse it
-            // reverse(temp.begin(),temp.end());
             return;
         }
         unordered_set<string> prevLevel = levelWords[currentLevel-1];

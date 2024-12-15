8a01d2cc6c2a49232636856337ed94e0473525e5
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-12-15 10:03:07 +0530

    Time: 7 ms (100.00%) | Memory: 82.2 MB (66.67%) - LeetSync

---
+++ 3613-maximize-amount-after-two-days-of-conversions/maximize-amount-after-two-days-of-conversions.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    unordered_map<string,vector<pair<string,double>>> adj;
+    unordered_map<string,double> convert1;
+    unordered_map<string,bool> visited;
+    void dfs1(string node, double cost) {
+        visited[node] = true;
+        for(auto &child : adj[node]) {
+            string str = child.first;
+            double wt = child.second;
+            if(!visited[str]) {
+                convert1[str] = max(convert1[str], cost * wt);
+                dfs1(str, cost * wt);
+            }
+        }
+    }
+    void dfs2(string node, double cost, string dst, double &res, string parent) {
+        if(node == dst) res = max(res, cost);
+        for(auto &child : adj[node]) {
+            string str = child.first;
+            double wt = child.second;
+            if(str != parent) {
+                dfs2(str, cost * wt, dst, res, node);
+            }
+        }
+    }
+    
+    double maxAmount(string initialCurrency, vector<vector<string>>& pairs1, vector<double>& rates1, vector<vector<string>>& pairs2, vector<double>& rates2) {
+        int n1 = rates1.size();
+        int n2 = rates2.size();
+        for(int i = 0;i < n1;i++) {
+            adj[pairs1[i][0]].push_back({pairs1[i][1], rates1[i]});
+            adj[pairs1[i][1]].push_back({pairs1[i][0], 1/rates1[i]});
+        }
+        dfs1(initialCurrency, 1);
+        adj.clear();
+        for(int i = 0;i < n2;i++) {
+            adj[pairs2[i][0]].push_back({pairs2[i][1], rates2[i]});
+            adj[pairs2[i][1]].push_back({pairs2[i][0], 1/rates2[i]});
+        }
+        visited.clear();
+        double ma = 0.0;
+        for(auto &[start,cst] : convert1) {
+            double res = 1;
+            dfs2(start, cst, initialCurrency, res, start);
+            ma = max(ma, res);
+        }
+        return ma;
+    }
+};

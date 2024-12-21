7d609ccdc825e0292b6c2bf02e8afeabdbcdac26
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-12-21 07:08:49 +0530

    Time: 170 ms (41.73%) | Memory: 11.3 MB (86.07%) - LeetSync

--- 301-remove-invalid-parentheses/remove-invalid-parentheses.cpp
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     //left = +1
     //right = -1
+    //to store unique permutations
     unordered_set<string> res;
     int ma = 0;
     void backtrack(string &s, int pos, int left, int right, string &temp) {
         int n = s.length();
         if(pos >= n) {
             if(left == right) {
+                //if a new string which has max length then previous are less than this
+                //so remove all of them and update max
                 if(temp.length() > ma) {
                     res.clear();
                     ma = temp.length();
                 }
+                //if my current string has length equal to max then store it
                 if(temp.length() == ma) res.insert(temp);
             }
             return;
@@ -23,6 +27,7 @@ public:
             backtrack(s, pos+1, left, right, temp);
         }
         else if(s[pos] == ')') {
+            //right braces should always be <= left braces
             if(left > right) {
                 temp += ')';
                 backtrack(s, pos+1, left, right + 1, temp);

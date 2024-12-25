cf255f02b526b00c37775f3cba921052a233fe72
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-12-25 18:54:54 +0530

    Time: 636 ms (35.26%) | Memory: 91.5 MB (5.06%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    unordered_map<string,int> dp;
     bool solve(vector<int> &nums, int k, int pos, int target, long long sum, int mask) {
         int n = nums.size();
         if(k == 0) {
@@ -11,11 +12,17 @@ public:
             return solve(nums, k-1, 0, target, 0, mask);
         }
         if(sum > target) return false;
+        string key = to_string(pos) + to_string(target) + to_string(sum) + to_string(mask);
+        if(dp.find(key) != dp.end()) return dp[key];
+        bool flag = false;
         for(int i = pos;i < n;i++) {
             if(mask & (1 << i)) continue;
-            if(solve(nums, k, i, target, sum + nums[i], mask | (1 << i))) return true;
+            if(solve(nums, k, i, target, sum + nums[i], mask | (1 << i))) {
+                flag = true;
+                break;
+            }
         }
-        return false;
+        return dp[key] = flag;
     }
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         int n = nums.size();

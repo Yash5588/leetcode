1c9e79558b91aea6c03a93e0d702b46ea94e1337
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-12-25 13:28:20 +0530

    Time: 4 ms (91.95%) | Memory: 10.6 MB (19.98%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/beautiful-arrangement.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
+    int dp[1 << 15][20];
     int solve(int n, int mask, int currPos) {
         if(currPos > n) return 1;
+        if(dp[mask][currPos] != -1) return dp[mask][currPos];
         int cnt = 0;
         for(int i = 1;i <= n;i++) {
             if(mask & (1 << (i-1))) continue;
@@ -9,9 +11,10 @@ public:
                 cnt += solve(n, mask | (1 << (i-1)), currPos+1);
             }
         }
-        return cnt;
+        return dp[mask][currPos] = cnt;
     }
     int countArrangement(int n) {
+        memset(dp,-1,sizeof(dp));
         return solve(n, 0, 1);
         
     }

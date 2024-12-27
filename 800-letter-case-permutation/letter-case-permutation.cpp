07ec82e3fc4dfd3deecc0e3ae131d162e81a8a14
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2024-12-27 22:20:16 +0530

    Time: 17 ms (18.24%) | Memory: 13.6 MB (79.23%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.cpp
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    unordered_set<string> res;
-    void solve(string &s, int pos) {
-        res.insert(s);
-        int n = s.length();
-        if(pos >= n) return;
-        if(s[pos] >= '0' && s[pos] <= '9') {
-            solve(s, pos+1);
-        }
-        else {
-            if(s[pos] >= 'a' && s[pos] <= 'z') {
-                char temp = s[pos];
-                s[pos] = (char)toupper(s[pos]);
-                solve(s, pos+1);
-                s[pos] = temp;
-            }
-            else {
-                char temp = s[pos];
-                s[pos] = (char)tolower(s[pos]);
-                solve(s, pos+1);
-                s[pos] = temp;
-            }
-            solve(s, pos+1);
-        }
-    }
     vector<string> letterCasePermutation(string s) {
-        solve(s,0);
+        //get all possible bit combinations
+        int n = s.length();
+        unordered_set<string> res;
+        res.insert(s);
+        for(int i = 0;i <= pow(2,n);i++) {
+            string t = s;
+            for(int j = 0;j < n;j++) {
+                if(i & (1 << j)) {
+                    if(s[j] >= '0' && s[j] <= '9') continue;
+                    if(s[j] >= 'a' && s[j] <= 'z') {
+                        char temp = s[j];
+                        s[j] = (char)toupper(s[j]);
+                        res.insert(s);
+                    }
+                    else {
+                        char temp = s[j];
+                        s[j] = (char)tolower(s[j]);
+                        res.insert(s);
+                    }
+                }
+            }
+            s = t;
+        }
         return vector<string> (res.begin(),res.end());
     }
 };

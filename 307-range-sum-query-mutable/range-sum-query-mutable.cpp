50dcfcddb8f518384f7e8246d898e162f7a6b4f9
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-01-02 00:01:16 +0530

    Time: 44 ms (86.19%) | Memory: 182.4 MB (25.29%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -2,40 +2,52 @@ class SegmentTree{
     vector<int> segTree;
     public:
         SegmentTree(int n) {
+            //it's a full binary tree so make size this much
             segTree.resize(4*n,0);
         }
 
         void buildTree(int i, int left, int right, vector<int> &nums) {
-            if(left == right) {
+            if(left == right) { //left node case 
                 segTree[i] = nums[left];
                 return;
             }
 
+            //left half  and right half of range sum 
             int mid = (left + right)/2;
+            //left half is present in left child 2*i+1
             buildTree(2*i+1, left, mid, nums);
+            //right half is present in right child 2*i+2;
             buildTree(2*i+2, mid+1, right, nums);
+            //left child and right child sum add to current index
             segTree[i] = segTree[2*i+1] + segTree[2*i+2];
         }
 
         void update(int idx, int val, int i, int left, int right) {
-            if(left == right) {
+            //to update find the index first
+            if(left == right) { //index found then update segTree
                 segTree[i] = val;
                 return;
             }
 
             int mid = (left + right)/2;
             if(idx <= mid) {
+                //if present in left side move to left side
                 update(idx, val, 2*i+1, left, mid);
             }
             else {
+                //if present in right side move to right side
                 update(idx, val, 2*i+2, mid+1, right);
             }
+            //update the corresponding path while backtracking
             segTree[i] = segTree[2*i+1] + segTree[2*i+2];
         }
 
         int rangeSumQuery(int start, int end, int i, int left, int right) {
+            //if target range is out of present range then we can't find sum here
             if(end < left || start > right) return 0;
+            //if present range is inside target range then return entire sum 
             if(start <= left && right <= end) return segTree[i];
+            //if there is some overlapping find in both sides
             int mid = (left + right)/2;
             return rangeSumQuery(start, end, 2*i+1, left, mid) + rangeSumQuery(start, end, 2*i+2, mid+1, right);
         }

d7ab5baa3b981dee9d2f4dac52c025bd7aade513
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-01-21 22:29:21 +0530

    Time: 2 ms (87.79%) | Memory: 86.2 MB (71.50%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -11,18 +11,32 @@
  */
 class Solution {
 public:
-    vector<int> res;
-    void inorder(TreeNode* root) {
-        if(root) {
-            inorder(root->left);
-            res.push_back(root->val);
-            inorder(root->right);
-        }
-    }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        inorder(root1);
-        inorder(root2);
-        sort(res.begin(),res.end());
+        stack<TreeNode*> sta1,sta2;
+        vector<int> res;
+        
+        while(root1 || root2 || !sta1.empty() || !sta2.empty()) {
+            while(root1 != nullptr) {
+                sta1.push(root1);
+                root1 = root1->left;
+            }
+            while(root2 != nullptr) {
+                sta2.push(root2);
+                root2 = root2->left;
+            }
+            if(sta2.empty() || (!sta1.empty() && sta1.top()->val <= sta2.top()->val)) {
+                root1 = sta1.top();
+                sta1.pop();
+                res.push_back(root1->val);
+                root1 = root1->right;
+            }
+            else {
+                root2 = sta2.top();
+                sta2.pop();
+                res.push_back(root2->val);
+                root2 = root2->right;
+            }
+        }
         return res;
     }
 };

3f0169ad7d430aba9b1cf50a8b05941754950faa
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-01-21 22:31:42 +0530

    Time: 14 ms (20.18%) | Memory: 86.2 MB (71.50%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -11,11 +11,15 @@
  */
 class Solution {
 public:
+    //TC : O(n)
+    //SC : O(n)
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
         stack<TreeNode*> sta1,sta2;
         vector<int> res;
-        
+
+        //here we traverse by taking both the roots simultaneously
         while(root1 || root2 || !sta1.empty() || !sta2.empty()) {
+            //move to the left mode nodes in both trees
             while(root1 != nullptr) {
                 sta1.push(root1);
                 root1 = root1->left;
@@ -24,6 +28,12 @@ public:
                 sta2.push(root2);
                 root2 = root2->left;
             }
+            //from there backtrack
+            //so while backtracking we check node values
+            //and also also stacks
+            //lesser side stack value we add into result
+            //and move to right side
+            //just like morris traversal
             if(sta2.empty() || (!sta1.empty() && sta1.top()->val <= sta2.top()->val)) {
                 root1 = sta1.top();
                 sta1.pop();

3f6e2549aff2789082f39f4f546c2bdb1f0b18c6
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-01-22 07:15:52 +0530

    Time: 54 ms (25.53%) | Memory: 108.4 MB (13.03%) - LeetSync

--- 1622-max-value-of-equation/max-value-of-equation.cpp
+++ 1622-max-value-of-equation/max-value-of-equation.cpp
@@ -3,12 +3,20 @@ public:
     int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
         int n = points.size();
         int ma = INT_MIN;
-        priority_queue<pair<int,int>> que;
+
+        //given equation = yi + yj + |xi - xj|
+        //now xj > xi so eq is (yi + yj + xj - xi)
+        //now segregate them ((yi - xi) + (xj + yj)) for i < j
+
+        priority_queue<pair<int,int>> que; //{yj-xj, xj} for all j
         for(auto point : points) {
+            //eliminate all top values which are of no use or not in range
             while(!que.empty() && point[0] - que.top().second > k) que.pop();
+            //get top most value which is in range
             if(!que.empty()) {
                 ma = max(ma, que.top().first + point[0] + point[1]);
             }
+            //store in que 
             que.push({point[1]-point[0] , point[0]});
         }
         return ma;

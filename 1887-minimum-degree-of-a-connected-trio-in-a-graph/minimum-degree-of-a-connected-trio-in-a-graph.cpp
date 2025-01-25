09e60117e470f11c130c6c0bbe6f5327e809d9a7
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-01-25 07:59:15 +0530

    Time: 239 ms (45.85%) | Memory: 47.4 MB (69.17%) - LeetSync

---
+++ 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minTrioDegree(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n+1, vector<int> (n+1,0));
+        vector<int> degree(n+1,0);
+        for(auto &edge : edges) {
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+            adj[edge[0]][edge[1]] = 1;
+            adj[edge[1]][edge[0]] = 1;
+        }
+
+        int mi = INT_MAX;
+        //check if there is an edge bet each node of list extra
+        for(int i = 1;i <= n;i++) {
+            for(int j = 1;j < n;j++) {
+                for(int k = j+1;k <= n;k++) {
+                    if(adj[i][j] == 1 && adj[i][k] == 1 && adj[j][k] == 1) {
+                        mi = min(mi, degree[i] + degree[j] + degree[k] - 6);
+                    }
+                }
+            }
+        }
+        return mi == INT_MAX ? -1 : mi;
+    }
+};

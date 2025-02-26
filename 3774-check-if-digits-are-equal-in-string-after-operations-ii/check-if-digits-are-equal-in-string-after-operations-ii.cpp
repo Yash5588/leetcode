29acde739f6a70ee063abc61d7e9b46aca2b41fc
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-02-26 18:42:18 +0530

    Time: 1304 ms (7.52%) | Memory: 50.9 MB (12.59%) - LeetSync

--- 3774-check-if-digits-are-equal-in-string-after-operations-ii/check-if-digits-are-equal-in-string-after-operations-ii.cpp
+++ 3774-check-if-digits-are-equal-in-string-after-operations-ii/check-if-digits-are-equal-in-string-after-operations-ii.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    int modInverse(int a, int mod) {
-        for (int i = 1; i < mod; i++) {
-            if ((a * i) % mod == 1) return i;
-        }
-        return 1;
-    }
 
 int nCrModpDP(int n, int r, int p) 
 { 
-    // The array C is going to store last row of 
-    // pascal triangle at the end. And last entry 
-    // of last row is nCr 
     int C[r+1]; 
     memset(C, 0, sizeof(C)); 
   
-    C[0] = 1; // Top row of Pascal Triangle 
-  
-    // One by constructs remaining rows of Pascal 
-    // Triangle from top to bottom 
+    C[0] = 1;
+
     for (int i = 1; i <= n; i++) 
     { 
-        // Fill entries of current row using previous 
-        // row values 
         for (int j = min(i, r); j > 0; j--) 
   
-            // nCj = (n-1)Cj + (n-1)C(j-1); 
             C[j] = (C[j] + C[j-1])%p; 
     } 
     return C[r]; 
@@ -34,18 +20,12 @@ int nCrModpDP(int n, int r, int p)
 
     int nCrModpLucas(int n, int r, int p) 
 { 
-   // Base case 
    if (r==0) 
       return 1; 
   
-   // Compute last digits of n and r in base p 
    int ni = n%p, ri = r%p; 
-  
-   // Compute result for last digits computed above, and 
-   // for remaining digits.  Multiply the two results and 
-   // compute the result of multiplication in modulo p. 
-   return (nCrModpLucas(n/p, r/p, p) * // Last digits of n and r 
-           nCrModpDP(ni, ri, p)) % p;  // Remaining digits 
+   return (nCrModpLucas(n/p, r/p, p) * 
+           nCrModpDP(ni, ri, p)) % p; 
 } 
 
     

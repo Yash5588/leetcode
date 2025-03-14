c28d6ad3e7ff6b18897e4cb415314e038651b05c
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-03-14 09:50:35 +0530

    Time: 432 ms (31.91%) | Memory: 194.1 MB (30.84%) - LeetSync

---
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    unordered_map<int,vector<int>> adj;
+    int topoSort(int n, string &colors) {
+        vector<int> degree(n,0);
+        for(int i = 0;i < n;i++) {
+            for(auto &child : adj[i]) {
+                degree[child]++;
+            }
+        }
+        queue<int> que;
+
+        int mxm = INT_MIN;
+        int cnt = 0;
+        vector<vector<int>> freq_dp(n, vector<int> (26,0));
+
+        for(int i = 0;i < n;i++) {
+            if(degree[i] == 0) {
+                que.push(i);
+            } 
+        }
+
+
+        while(!que.empty()) {
+
+            int node = que.front();
+            que.pop();
+
+            freq_dp[node][colors[node] - 'a']++;
+
+            for(int i = 0;i < 26;i++) {
+                mxm = max(mxm, freq_dp[node][i]);
+            }
+
+            cnt++;
+
+            for(auto &child : adj[node]) {
+
+
+                for(int i = 0;i < 26;i++) {
+                    freq_dp[child][i] = max(freq_dp[child][i], freq_dp[node][i]);
+                }
+
+                degree[child]--;
+                if(degree[child] == 0) {
+                    que.push(child);
+                }
+            }
+        }
+        if(cnt != n) return -1;
+        return mxm;
+    }
+
+    int largestPathValue(string colors, vector<vector<int>>& edges) {
+        for(auto &edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+        }
+        int n = colors.size();
+        return topoSort(n,colors);
+    }
+};

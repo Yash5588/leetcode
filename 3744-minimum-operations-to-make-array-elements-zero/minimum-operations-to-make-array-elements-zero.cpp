3178b8b404b1a02ac306ca8e5e6d901952350a7e
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-03-23 17:41:10 +0530

    Time: 14 ms (76.04%) | Memory: 183.3 MB (70.14%) - LeetSync

---
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long minOperations(vector<vector<int>>& queries) {
+        long long res = 0;
+        for(auto &q : queries) {
+            long long l = q[0];
+            long long r = q[1];
+            long long sum = 0;
+            int k = 0;
+            long long low = 1;
+            while(low <= r) {
+                long long high = low * 4;
+                if(low > r) break;
+                long long start = max(low, l);
+                long long end = min(high-1, r);
+                if(start <= end) {
+                    long long cnt = end - start + 1;
+                    long long opers = (k+1)*cnt;
+                    sum += opers;
+                }
+                k++;
+                low = high;
+            }
+            res += (sum + 1)/2;
+        }
+        return res;
+    }
+};

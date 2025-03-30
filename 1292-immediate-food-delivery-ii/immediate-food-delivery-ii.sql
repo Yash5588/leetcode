c65c5479cb05111513cbb32ce4fbe382c798d62a
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-03-30 22:16:50 +0530

    Time: 624 ms (94.19%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT ROUND((SUM(IF(order_date = customer_pref_delivery_date, 1 , 0)) / COUNT(*)) * 100.00, 2) 
+SELECT ROUND(AVG(IF(order_date = customer_pref_delivery_date, 1 , 0)) * 100.00, 2) 
 AS immediate_percentage 
 FROM DElivery
 WHERE (customer_id, order_date) IN (

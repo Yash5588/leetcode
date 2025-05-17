a20443dadd5e4d66dd09462fd8fd1e4a522779f5
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-05-17 18:04:57 +0530

    Time: 872 ms (33.26%) | Memory: 494 MB (27.23%) - LeetSync

--- 3784-longest-common-prefix-of-k-strings-after-removal/longest-common-prefix-of-k-strings-after-removal.cpp
+++ 3784-longest-common-prefix-of-k-strings-after-removal/longest-common-prefix-of-k-strings-after-removal.cpp
@@ -16,11 +16,11 @@ struct Node {
     }
 };
 
-multiset<int, greater<int>> mset;
+multiset<int,greater<int>> mset;
 
-class Trie {
+class Trie{
     Node* root = nullptr;
-public:
+    public:
     Trie() {
         root = new Node();
     }
@@ -28,54 +28,50 @@ public:
     void insert(string &word, int k) {
         Node* node = root;
         int n = word.size();
-        for (int i = 0; i < n; i++) {
+        for(int i = 0;i < n;i++) {
             int idx = word[i] - 'a';
-            if (!node->isContains(idx)) {
+            if(!node->isContains(idx)) {
                 node->put(idx, new Node());
             }
             node = node->get(idx);
             node->count++;
-            if (node->count >= k) {
-                mset.insert(i + 1);
-            }
+            if(node->count >= k) mset.insert(i+1);
         }
         node->flag = true;
     }
 
     void delete_word(string &word, int k) {
-        Node* node = root;
         int n = word.size();
-        for (int i = 0; i < n; i++) {
+        Node* node = root;
+        for(int i = 0;i < n;i++) {
             int idx = word[i] - 'a';
             node = node->get(idx);
-            if (node->count >= k) {
-                mset.erase(mset.find(i + 1));
+            if(node->count >= k) {
+                mset.erase(mset.find(i+1));
             }
             node->count--;
         }
         node->flag = false;
     }
 };
-
 class Solution {
 public:
     vector<int> longestCommonPrefix(vector<string>& words, int k) {
         mset.clear();
         int n = words.size();
         Trie trie;
-        for (auto &word : words) {
-            trie.insert(word, k);
-        }
+        for(auto &word : words) trie.insert(word,k);
         vector<int> res;
-        for (int i = 0; i < n; i++) {
-            trie.delete_word(words[i], k);
-            if (mset.empty()) {
-                res.push_back(0);
-            } else {
-                res.push_back(*mset.begin());
-            }
-            trie.insert(words[i], k);
+        for(int i = 0;i < n;i++) {
+            trie.delete_word(words[i],k);
+            if(mset.empty()) res.push_back(0);
+            else res.push_back(*mset.begin());
+            trie.insert(words[i],k);
         }
+        // for(auto &x : mset) cout << x << endl;
+        // trie.delete_word(words[0],k);
+        // cout << endl;
+        // for(auto &x : mset) cout << x << endl;
         return res;
     }
 };

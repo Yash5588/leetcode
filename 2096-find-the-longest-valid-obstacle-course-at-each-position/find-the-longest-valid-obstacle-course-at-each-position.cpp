529dc3aeff688b519974cc04cae9f485d2a3d3f6
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-05-19 14:42:11 +0530

    Time: 155 ms (7.01%) | Memory: 301 MB (5.14%) - LeetSync

--- 2096-find-the-longest-valid-obstacle-course-at-each-position/find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 2096-find-the-longest-valid-obstacle-course-at-each-position/find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -1,18 +1,43 @@
+class FenwickTree{
+    private:
+        vector<int> bit;
+        int N;
+    public:
+        FenwickTree(int n) {
+            bit.resize(n+1,0);
+            N = n+1;
+        }
+
+        int query(int id) {
+            int ans = 0;
+            while(id > 0) {
+                ans = max(ans, bit[id]);
+                id -= (id & -id);
+            }
+            return ans;
+        }
+
+        void update(int id, int val) {
+            while(id < N) {
+                bit[id] = max(bit[id], val);
+                id += (id & -id);
+            }
+        }
+};
+
 class Solution {
 public:
     vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
         int n = obstacles.size();
-        vector<int> store, res;
+        vector<int> res(n);
+        int maxx = *max_element(obstacles.begin(), obstacles.end());
+        FenwickTree fen(maxx);
         for(int i = 0;i < n;i++) {
-            int idx = upper_bound(store.begin(), store.end(), obstacles[i]) - store.begin();
-            if(idx == store.size()) {
-                res.push_back(store.size()+1);
-                store.push_back(obstacles[i]);
-            }
-            else {
-                res.push_back(idx+1);
-                store[idx] = obstacles[i];
-            }
+            int idx = obstacles[i];
+            //ascending so till that point
+            int max_len = fen.query(idx) + 1;
+            res[i] = max_len;
+            fen.update(idx, max_len);
         }
         return res;
     }

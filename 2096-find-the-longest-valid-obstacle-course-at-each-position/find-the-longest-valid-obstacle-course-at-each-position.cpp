4a5697c54c61c61d430916603d8eafb158ba1dc8
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-05-19 19:42:03 +0530

    Time: 314 ms (6.31%) | Memory: 166.9 MB (5.14%) - LeetSync

--- 2096-find-the-longest-valid-obstacle-course-at-each-position/find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 2096-find-the-longest-valid-obstacle-course-at-each-position/find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -1,43 +1,53 @@
-class FenwickTree{
-    private:
-        vector<int> bit;
-        int N;
+class SegmentTree{
+    vector<int> segTree;
     public:
-        FenwickTree(int n) {
-            bit.resize(n+1,0);
-            N = n+1;
+        SegmentTree(int n) {
+            segTree.resize(4*n, 0);
         }
 
-        int query(int id) {
-            int ans = 0;
-            while(id > 0) {
-                ans = max(ans, bit[id]);
-                id -= (id & -id);
+        void update(int idx, int val, int i, int left, int right) {
+            if(left == right) {
+                segTree[i] = val;
+                return;
             }
-            return ans;
+            int mid = (left + right)/2;
+            if(idx <= mid) {
+                update(idx, val, 2*i+1, left, mid);
+            }
+            else {
+                update(idx, val, 2*i+2, mid+1, right);
+            }
+            segTree[i] = max(segTree[2*i+1], segTree[2*i+2]);
         }
 
-        void update(int id, int val) {
-            while(id < N) {
-                bit[id] = max(bit[id], val);
-                id += (id & -id);
-            }
+        int rangeMaxQuery(int start, int end, int i, int left, int right) {
+            if(right < start || left > end) return 0;
+            if(start <= left && right <= end) return segTree[i];
+            int mid = (left + right)/2;
+            return max(rangeMaxQuery(start,end, 2*i+1, left, mid), rangeMaxQuery(start, end, 2*i+2, mid+1, right));
         }
 };
-
 class Solution {
 public:
     vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
         int n = obstacles.size();
-        vector<int> res(n);
-        int maxx = *max_element(obstacles.begin(), obstacles.end());
-        FenwickTree fen(maxx);
-        for(int i = 0;i < n;i++) {
-            int idx = obstacles[i];
-            //ascending so till that point
-            int max_len = fen.query(idx) + 1;
-            res[i] = max_len;
-            fen.update(idx, max_len);
+        unordered_map<int,int> indices;
+        vector<int> temp;
+        temp = obstacles;
+        sort(temp.begin(),temp.end());
+        temp.erase(unique(temp.begin(),temp.end()), temp.end());
+        int size = temp.size();
+        for(int i = 0;i < size;i++) {
+            indices[temp[i]] = i;
+        }
+
+        SegmentTree seg(size);
+        vector<int> res;
+        for(auto &x : obstacles) {
+            int idx = indices[x];
+            int max_len = seg.rangeMaxQuery(0,idx,0,0,size-1) + 1;
+            res.push_back(max_len);
+            seg.update(idx, max_len, 0, 0, size-1);
         }
         return res;
     }

ec4a95cf686d56249b5a8248accea0f352030733
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-07-19 21:00:59 +0530

    Time: 39 ms (61.85%) | Memory: 14 MB (43.16%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dp[100001];
+    //true - alice wins
+    //false - bob wins
+    //draw tree to understand better
+    int solve(int n) {
+        if(n == 0) return false;
+        if(dp[n] != -1) return dp[n];
+        for(int i = 1;i*i <= n;i++) {
+            if(solve(n - i*i) == false) {
+                return dp[n] = true;
+            }
+        }
+        return dp[n] = false;
+    }
+    bool winnerSquareGame(int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n);
+    }
+};

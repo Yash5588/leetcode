6474ca7e8bd978ad2166db6ef1cd768940d0c8b9
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-08-17 18:29:33 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (12.22%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        map<int,int> mp;
+        for(auto trip: trips) {
+            mp[trip[1]] += trip[0];
+            mp[trip[2]] -= trip[0];
+        }
+        int members = 0;
+        for(auto &[loc,num] : mp) {
+            members += num;
+            if(members < 0) return false;
+            if(members > capacity) return false;
+        }
+        return true;
+    }
+};

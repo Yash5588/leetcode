fb384ffb35fc51b034913d2d0b217d8ef74b8880
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-08-27 18:34:05 +0530

    Time: 10 ms (86.82%) | Memory: 16.5 MB (96.50%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    unordered_map<int,vector<pair<int,int>>> adj;
-    int count_dijkstra(int src, int n, int threshold) {
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> que;
-        que.push({0, src});
-        while(!que.empty()) {
-            int node = que.top().second;
-            int dis = que.top().first;
-            que.pop();
-            for(auto &x : adj[node]) {
-                int dest_node = x.first;
-                int edgeWeight = x.second;
-                if(dis + edgeWeight < dist[dest_node]) {
-                    dist[dest_node] = dis + edgeWeight;
-                    que.push({dist[dest_node], dest_node});
+    void floyd_warshall(vector<vector<int>> &adj_matrix) {
+        int n = adj_matrix.size();
+        for(int i = 0;i < n;i++) {
+            adj_matrix[i][i] = 0;
+        }
+
+        for(int k = 0;k < n;k++) {
+            for(int i = 0;i < n;i++) {
+                for(int j = 0;j < n;j++) {
+                    adj_matrix[i][j] = min(adj_matrix[i][j], adj_matrix[i][k] + adj_matrix[k][j]);
                 }
             }
         }
-        int cnt = 0;
-        for(int i = 0;i < n;i++) {
-            if(dist[i] <= threshold && i != src) cnt++;
-        }
-        return cnt;
+
+
     }
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> adj_matrix(n, vector<int>(n,1e9));
         for(auto &edge : edges) {
-            adj[edge[0]].push_back({edge[1],edge[2]});
-            adj[edge[1]].push_back({edge[0],edge[2]});
+            int u = edge[0];
+            int v = edge[1];
+            adj_matrix[u][v] = edge[2];
+            adj_matrix[v][u] = edge[2];
         }
+        floyd_warshall(adj_matrix);
         int mnm = INT_MAX;
         int city = 0;
         for(int i = 0;i < n;i++) {
-            int cnt = count_dijkstra(i,n,distanceThreshold);
+            int cnt = 0;
+            for(int j = 0;j < n;j++) {
+                if(distanceThreshold >= adj_matrix[i][j] && i != j) {
+                    cnt++;
+                }
+            }
             if(mnm >= cnt) {
                 mnm = cnt;
                 city = i;

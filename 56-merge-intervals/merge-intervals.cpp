7f6f5e271886e8357a76f86c4d38cc3de2220f1f
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2025-09-06 19:11:43 +0530

    Time: 6 ms (47.72%) | Memory: 24 MB (30.89%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-    bool isMerged(vector<int> &int1, vector<int> &int2) {
-        if(int1[0] <= int2[0] && int2[0] <= int1[1]) return true;
-        if(int1[0] <= int2[1] && int2[1] <= int1[1]) return true;
-        if(int2[0] <= int1[0] && int1[0] <= int2[1]) return true;
-        if(int2[0] <= int1[1] && int1[1] <= int2[1]) return true;
-        return false;
-    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end());
         int n = intervals.size();
-        sort(intervals.begin(),intervals.end());
-        int i = 0,j;
-        while(i < n) {
-            j = i+1;
-            while(j < n && isMerged(intervals[i],intervals[j])) {
-                intervals[i][0] = min(intervals[i][0], intervals[j][0]);
-                intervals[i][1] = max(intervals[i][1], intervals[j][1]);
-                j++;
+        vector<vector<int>> res;
+        int idx = 0;
+        while(idx < n) {
+            res.push_back(intervals[idx]);
+            while(idx < n && intervals[idx][0] <= res.back()[1]) {
+                res.back()[1] = max(res.back()[1], intervals[idx][1]);
+                idx++;
             }
-            res.push_back(intervals[i]);
-            i = j;
         }
         return res;
     }

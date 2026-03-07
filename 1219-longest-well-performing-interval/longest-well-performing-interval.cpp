5e6013dc04dc4726ef2190c02a5e806f50b7413c
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-03-07 10:05:33 +0530

    Time: 6 ms (66.18%) | Memory: 26.9 MB (68.57%) - LeetSync

---
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestWPI(vector<int>& hours) {
+        int n=hours.size(),mx=0,count=0;
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<n;i++){
+            if(hours[i]>8) count++;
+            else count--;
+
+            if(count>0){
+                mx=max(mx,i+1);
+                continue;
+            }else{
+                if(!mp.count(count)){
+                    mp[count]=i;
+                }
+            }
+            if(mp.count(count-1)){
+                mx=max(mx,i-mp[count-1]);
+            }
+        }
+        return mx;
+    }
+};

f021c73114cf41c5685b38b1fbf991a8f2d51698
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-03-07 10:02:29 +0530

    Time: 2 ms (70.31%) | Memory: 9.3 MB (79.73%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
+    //M-1000
+    //CM-900
+    //D-500
+    //CD-400
+    //C-100
+    //XC-90
+    //L-50
+    //XL-40
+    //X-10
+    //IX-9
+    //V-5
+    //IV-4
+    //I-1
     string intToRoman(int num) {
-        const vector<pair<int, string>> valueSymbols{
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
-            {90, "XC"},  {50, "L"},   {40, "XL"}, {10, "X"},   {9, "IX"},
-            {5, "V"},    {4, "IV"},   {1, "I"}};
+        string s="";
+        int intCode[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string stCode[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        for(int i=0;i<13;i++){
+            while(num>=intCode[i]){
+                s+=stCode[i];
+                num-=intCode[i];
 
-        string res;
-
-        for (const auto& [value, symbol] : valueSymbols) {
-            if (num == 0)
-                break;
-
-            while (num >= value) {
-                res += symbol;
-                num -= value;
             }
         }
-
-        return res;        
+     
+        return s;
+        
     }
 };

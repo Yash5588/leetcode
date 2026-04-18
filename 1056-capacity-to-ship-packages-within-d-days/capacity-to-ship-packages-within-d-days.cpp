fcfe2e673615584b4989abae9b3177953c325eec
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-04-18 16:57:08 +0530

    Time: 12 ms (52.08%) | Memory: 35.1 MB (19.72%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    int getDays(vector<int> &wts, int days, int limit) {
-        int n = wts.size();
-        int cnt = 1;
-        int val = 0;
-        for(int i = 0;i < n;i++) {
-            if(val + wts[i] <= limit) {
-                val += wts[i];
+    bool valid(int capacity, vector<int> &weights, int days) {
+        long long total = 0;
+        long long sum = 0;
+        for(auto &x : weights) {
+            if(sum + x <= capacity) {
+                sum += x;
             }
             else {
-                cnt++;
-                val = wts[i];
+                sum = x;
+                total++;
             }
         }
-        return cnt;
+        total++;
+        return total <= days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int n = weights.size();
         int low = *max_element(weights.begin(),weights.end());
-        int high = 1e9;
-        int res = -1;
+        int high = accumulate(weights.begin(),weights.end(),0);
+        int ans = -1;
         while(low <= high) {
             int mid = low + (high - low)/2;
-            if(getDays(weights, days, mid) <= days) {
-                res = mid;
+            if(valid(mid,weights,days)) {
+                ans = mid;
                 high = mid - 1;
             }
             else low = mid + 1;
         }
-        return res;
+        return ans;
     }
 };

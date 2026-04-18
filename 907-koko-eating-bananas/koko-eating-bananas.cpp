f4eb09798dc8f9ffaf822847003a1185e06a33c1
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-04-18 16:26:27 +0530

    Time: 6 ms (74.63%) | Memory: 22.9 MB (44.82%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    //time to complete all the bananas for a fixed speed
-    long long time(vector<int> &piles, double speed) {
-        int n = piles.size();
-        long long t = 0;
-        for(int i = 0;i < n;i++) {
-            t += ceil(piles[i]/speed);
+    bool valid(int k, vector<int> &piles, int h) {
+        long long total = 0;
+        for(auto &x : piles) {
+            int val = (x+k-1)/k;
+            total += val;
         }
-        return t;
+        return total <= h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
-        //determine the speed by hit and trial
-        int low = 1,high = *max_element(piles.begin(),piles.end());
+        int low = 1, high = 1e9;
         int ans = -1;
         while(low <= high) {
             int mid = low + (high - low)/2;
-            //if it staisfies condtion it might be a possible result
-            //aim for better result
-            if(time(piles,mid) <= h) {
+            if(valid(mid, piles,h)) {
                 ans = mid;
                 high = mid - 1;
             }
-            else low = mid + 1;
+            else {
+                low = mid + 1;
+            }
         }
         return ans;
     }

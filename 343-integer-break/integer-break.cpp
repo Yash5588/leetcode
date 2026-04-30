5a3e1d0719383ae32a0bc1fb627389c3a69c7da8
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-04-30 07:05:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.26%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int dp[100][100];
-    int solve(int n, int k) {
+    int dp[100];
+    int solve(int n) {
         if(n == 0) {
-            if(k >= 2) return 1;
-            return -1;
+            // if(k >= 2) return 1;
+            return 1;
         }
-        if(dp[n][k] != -1) return dp[n][k];
+        if(dp[n] != -1) return dp[n];
         int mxm = INT_MIN;
         for(int i = n;i >= 1;i--) {
-            mxm = max(mxm, i * solve(n-i,k+1));
+            mxm = max(mxm, i * solve(n-i));
         }
-        return dp[n][k] = mxm;
+        return dp[n] = mxm;
     }
     int integerBreak(int n) {
         memset(dp,-1,sizeof(dp));
-        return solve(n, 0);
+        int mxm = INT_MIN;
+        for(int i = n-1;i >= 1;i--) {
+            mxm = max(mxm, i * solve(n-i));
+        }
+        return mxm;
     }
 };

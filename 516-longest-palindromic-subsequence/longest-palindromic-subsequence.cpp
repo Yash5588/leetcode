9af4d32d01dc763017bdbb7ee6f3c2a2ea15e7eb
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-05-01 15:24:36 +0530

    Time: 223 ms (5.12%) | Memory: 13.1 MB (84.40%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int solve(string &s1 ,string &s2, int pos1, int pos2) {
-        int n = s1.length();
-        if(pos1 >= n || pos2 >= n) return 0;
-        int ma = INT_MIN;
-        if(dp[pos1][pos2] != -1) return dp[pos1][pos2];
-        if(s1[pos1] == s2[pos2]) {
-            return dp[pos1][pos2] = 1 + solve(s1, s2, pos1+1, pos2+1);
+    int dp[1000][1000];
+    int solve(string &str1, string &str2, int idx1, int idx2) {
+        int n1 = str1.length();
+        int n2 = str2.length();
+        if(idx1 >= n1 || idx2 >= n2) return 0;
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        int pick = 0, not_pick = 0;
+        if(str1[idx1] == str2[idx2]) {
+            pick = 1 + solve(str1, str2, idx1+1, idx2+1);
         }
-        return dp[pos1][pos2] = max(solve(s1, s2, pos1+1, pos2), solve(s1, s2, pos1, pos2+1));
+        not_pick = max(solve(str1, str2, idx1+1, idx2), solve(str1, str2, idx1, idx2+1));
+        return dp[idx1][idx2] = max(pick, not_pick);
     }
     int longestPalindromeSubseq(string s) {
-        string s2 = s;
+        int n = s.length();
+        string rev = s;
         reverse(s.begin(),s.end());
         memset(dp,-1,sizeof(dp));
-        return solve(s,s2,0,0);
+        return solve(s, rev, 0, 0);
     }
 };

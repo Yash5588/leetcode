9b304bf7d3bb083946647fbd23e1b65ddc3c0d78
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-05-03 10:57:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (94.11%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    int dp[2501][2501];
-    int solve(vector<int> &nums, int idx, int prev) {
-        int n = nums.size();
-        if(idx >= n) return 0;
-        if(dp[idx][prev] != -1) return dp[idx][prev];
-        int pick = 0, not_pick = 0;
-        if(prev >= n || nums[prev] < nums[idx]) {
-            pick = 1 + solve(nums, idx+1, idx);
-        }
-        not_pick = solve(nums, idx+1, prev);
-        return dp[idx][prev] = max(pick, not_pick);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums, 0, nums.size());
+        vector<int> vec;
+        int n = nums.size();
+        for(int i = 0;i < n;i++) {
+            int ind = lower_bound(vec.begin(),vec.end(),nums[i]) - vec.begin();
+            if(ind == vec.size()) vec.push_back(nums[i]);
+            else vec[ind] = nums[i];
+        }
+        return vec.size();
     }
 };

4c678a2c8742c43d9780b542f5504d78f31ff78b
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-05-03 14:49:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (92.21%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int dp[100][100];
+    int solve(int m, int n, int row, int col) {
+        if(row >= m || col >= n) return 0;
+        if(row == m-1 && col == n-1) return 1;
+        if(dp[row][col] != -1) return dp[row][col];
+        return dp[row][col] = solve(m,n,row+1,col) + solve(m,n,row,col+1);
+    }
+    int uniquePaths(int m, int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(m,n,0,0);
+    }
+};

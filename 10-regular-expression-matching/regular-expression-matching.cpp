86fb1c3a7fe7071795595c1bd9dfb10ddfa559d9
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-05-03 10:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (78.52%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -4,19 +4,19 @@ public:
     bool solve(string &s, string &p,  int i, int j) {
         int n1 = s.length();
         int n2 = p.length();
-
         if(i >= n1 && j >= n2) return true;
         if(j >= n2) return false; 
+        if(dp[i][j] != -1) return dp[i][j];
         if(i < n1 && (s[i] == p[j] || p[j] == '.')) {
             if(j+1 < n2 && p[j+1] == '*') {
-                return solve(s,p,i+1,j) || solve(s,p,i,j+2);
+                return dp[i][j] = solve(s,p,i+1,j) || solve(s,p,i,j+2);
             }
-            return solve(s,p,i+1,j+1);
+            return dp[i][j] = solve(s,p,i+1,j+1);
         }
         if(j+1 < n2 && p[j+1] == '*') {
-            return solve(s,p,i,j+2);
+            return dp[i][j] = solve(s,p,i,j+2);
         }
-        return false;
+        return dp[i][j] = false;
     }
     bool isMatch(string s, string p) {
         memset(dp,-1,sizeof(dp));

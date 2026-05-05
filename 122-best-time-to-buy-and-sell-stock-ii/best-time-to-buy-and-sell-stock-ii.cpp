da86de01bb5e511c7689f7d165d8d30f7158b3a9
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-05-05 08:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (28.59%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     int dp[30002][2];
-
-    int solve(vector<int> &prices, int idx, int isBuy) {
-        if(idx >= prices.size()) return 0;
-
-        if(dp[idx][isBuy] != -1) return dp[idx][isBuy];
-
-        int profit = 0;
-
-        if(isBuy == 0) {
-            // buy
-            profit = max(
-                -prices[idx] + solve(prices, idx+1, 1),
-                solve(prices, idx+1, 0)
-            );
-        } else {
-            // sell
-            profit = max(
-                prices[idx] + solve(prices, idx+1, 0),
-                solve(prices, idx+1, 1)
-            );
+    int solve(vector<int> &prices, int idx, bool isBuy) {
+        int n = prices.size();
+        if(idx >= n) {
+            return 0;
         }
+        if(dp[idx][isBuy] != -1) return dp[idx][isBuy];
+        int mxm = INT_MIN;
+        if(!isBuy) {
+            mxm = max(mxm, -prices[idx] + solve(prices, idx+1, !isBuy));
+        }
+        else {
+            mxm = max(mxm, prices[idx] + solve(prices, idx+1, !isBuy));
+        }
+        mxm = max(mxm, solve(prices, idx+1, isBuy));
+        return dp[idx][isBuy] = mxm;
 
-        return dp[idx][isBuy] = profit;
     }
-
     int maxProfit(vector<int>& prices) {
-        memset(dp, -1, sizeof(dp));
-        return solve(prices, 0, 0);
+        memset(dp,-1,sizeof(dp));
+        return solve(prices, 0, false);
     }
 };

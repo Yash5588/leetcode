752de6e76761faafa46781934c2b2c36b561cb1c
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-05-10 15:58:00 +0530

    Time: 63 ms (57.29%) | Memory: 26.3 MB (20.99%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    bool isPalindrome(string &s) {
-        int n = s.length();
-        for(int i = 0;i < n/2;i++) {
-            if(s[i] != s[n-i-1]) return false;
-        }
-        return true;
-    }
     int dp[2000];
+    int pal_dp[2000][2000];
     int solve(string &s, int idx) {
         int n = s.length();
         if(idx >= n) return 0;
         if(dp[idx] != -1) return dp[idx];
-        string temp;
         int mnm = 1e5;
         for(int i = idx;i < n;i++) {
-            temp += s[i];
-            if(isPalindrome(temp)) {
+            if(isPal(s, idx, i)) {
                 mnm = min(mnm, 1 + solve(s, i+1));
             }
         }
         return dp[idx] = mnm;
     }
+    bool isPal(string &s, int start, int end) {
+        if(start >= end) return true;
+        if(pal_dp[start][end] != -1) return pal_dp[start][end];
+        if(s[start] == s[end]) {
+            return pal_dp[start][end] = isPal(s, start+1, end-1);
+        }
+        return pal_dp[start][end] = false;
+    }
     int minCut(string s) {
         memset(dp,-1,sizeof(dp));
+        memset(pal_dp, -1,sizeof(pal_dp));
         return solve(s,0)-1;
     }
 };

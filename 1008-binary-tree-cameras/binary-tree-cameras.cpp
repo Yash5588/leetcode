b9dc575cdd4b71b5e28aed755009a17c574ad14a
Yash5588 <146909995+Yash5588@users.noreply.github.com>
2026-05-10 13:51:26 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (90.49%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.cpp
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -11,40 +11,28 @@
  */
 class Solution {
 public:
-    map<pair<TreeNode*,pair<bool,bool>>,long long> dp;
-    bool isLeaf(TreeNode* root) {
-        return root->left == nullptr && root->right == nullptr;
-    }
-    long long solve(TreeNode* root, bool placeCam, bool parentCam) {
-        //if we get assign camera to null
-        //then that path is wrong
-        if(root == nullptr) {
-            if(placeCam) return INT_MAX;
-            return 0;
+    int cameras = 0;
+    int solve(TreeNode* root, TreeNode* parent) {
+        if(root == nullptr) return 1;
+
+        int left = solve(root->left, root);
+        int right = solve(root->right, root);
+
+        if(left == 0 || right == 0) {
+            cameras++;
+            return 2;
         }
-        if(dp.find({root,{placeCam,parentCam}}) != dp.end()) {
-            return dp[{root,{placeCam,parentCam}}];
-        }
-        //root is safe by either it's parent has camera
-        //or root has camera 
-        if(placeCam || parentCam) {
-            long long left = min(solve(root->left,0,placeCam),solve(root->left,1,placeCam));
-            long long right = min(solve(root->right,0,placeCam),solve(root->right,1,placeCam));
-            if(placeCam) return dp[{root,{placeCam,parentCam}}] = 1 + left + right;
-            return  dp[{root,{placeCam,parentCam}}] = left + right;
-        }
-        //this case it when root parent doesn't have camera 
-        //and root also doesn't have camera
+        else if(left == 2 || right == 2) return 1;
         else {
-            long long placeLeft,placeRight;
-            //we forcefully assign camera to one child
-            //and give choice to another
-            placeLeft = solve(root->left,1,placeCam) + min(solve(root->right,1,placeCam),solve(root->right,0,placeCam));
-            placeRight = solve(root->right,1,placeCam) + min(solve(root->left,1,placeCam),solve(root->left,0,placeCam));
-            return  dp[{root,{placeCam,parentCam}}] = min(placeLeft,placeRight);
+            if(parent == nullptr) {
+                cameras++;
+                return 2;
+            }
+            return 0;
         }
     }
     int minCameraCover(TreeNode* root) {
-        return min(solve(root,0,0),solve(root,1,0));
+        solve(root, nullptr);
+        return cameras;
     }
 };
